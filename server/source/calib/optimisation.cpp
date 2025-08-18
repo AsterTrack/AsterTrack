AsterTrack/AsterTrack

commit 949e15948d557e57cea69d0d1ca318712589da05

Reworked signal/update handling in pipeline Anything that can be stored and is updated by pipeline now: - Uses signals to copy them by value to server - Has a dirty flag that UI can use to remind user to save

server/source/calib_point/calibration_room.cpp
@@ -314,14 +314,22 @@ void getCalibNormalisation(const std::vector<CameraCalib_t<Scalar>> &calibs, Mat
 	Affine3<Scalar> &roomTransform, Scalar height, Scalar pairwiseDist)
 {
 	Vector3<Scalar> origin = Vector3<Scalar>::Zero();
+	int c = 0;
 	for (auto &calib : calibs)
+	{
+		if (calib.invalid()) continue;
 		origin += calib.transform.translation();
-	origin /= calibs.size();
+		c++;
+	}
+	if (c < 2) return; 
+	origin /= c;
 
 	// Perform Principle Component Analysis to get the least represented axis, which is our axis going vertically through the plane of the points
-	Eigen::Matrix3X<Scalar> N = Eigen::Matrix3X<Scalar>(3, calibs.size());
-	for (int c = 0; c < calibs.size(); c++)
-		N.col(c) = calibs[c].transform.translation()-origin;
+	Eigen::Matrix3X<Scalar> N = Eigen::Matrix3X<Scalar>(3, c);
+	c = 0;
+	for (auto &calib : calibs)
+		if (!calib.invalid())
+			N.col(c++) = calib.transform.translation()-origin;
 
 	// TODO: Choose either SVD or EVD
 	Eigen::JacobiSVD<Eigen::Matrix3X<Scalar>, Eigen::ComputeFullU> svd_N(N);
@@ -333,8 +341,9 @@ void getCalibNormalisation(const std::vector<CameraCalib_t<Scalar>> &calibs, Mat
 
 	// Flip room up axis based on camera positions
 	Scalar flipAxis = 0;
-	for (int c = 0; c < calibs.size(); c++)
-		flipAxis += axis.dot(calibs[c].transform.translation() - origin);
+	for (auto &calib : calibs)
+		if (!calib.invalid())
+			flipAxis += axis.dot(calib.transform.translation() - origin);
 	if (flipAxis < 0)
 		axis = -axis;
 

server/source/ui/pipeline.cpp
@@ -88,7 +88,7 @@ void InterfaceState::UpdatePipeline(InterfaceWindow &window)
 					}
 				}
 			}
-			UpdateCalibrations(false);
+			UpdateCalibrations();
 		}
 	}
 
@@ -340,14 +340,12 @@ void InterfaceState::UpdatePipeline(InterfaceWindow &window)
 
 					{ // Update calibration
 						std::unique_lock pipeline_lock(pipeline.pipelineLock);
-						AdoptRoomCalibration(pipeline, calibs);
-						for (int c = 0; c < calibs.size(); c++)
-							pipeline.cameras[calibs[c].index]->calib = calibs[c];
+						AdoptNewCalibrations(pipeline, calibs, true);
 					}
+					SignalCameraCalibUpdate(calibs);
 
-					// Update fundamental matrices using calibration
 					UpdateCalibrationRelations(pipeline, *pipeline.calibration.contextualLock(), lastError, data.getValidSamples());
-					UpdateCalibrations();
+
 				}, *db_lock);
 			}
 			ImGui::SameLine();
@@ -377,31 +375,19 @@ void InterfaceState::UpdatePipeline(InterfaceWindow &window)
 
 					LOGCL("Done optimising! Applying to targets!");
 
-					{ // Update Targets
-						for (auto &tgtNew : data.targets)
-						{
-							bool found = false;
-							for (auto &tracker : state.trackerConfigs)
-							{
-								if (tracker.id != tgtNew.targetID) continue;
-								updateMarkerOrientations(tracker.calib.markers, calibs, tgtNew, pipeline.targetCalib.params.post);
-								tracker.calib.updateMarkers();
-								if (tracker.isSimulated)
-								{ // Ensure calibrated, simulated targets are adopted as normal targets and stored
-									tracker.isSimulated = false;
-								}
-								ServerUpdatedTrackerConfig(state, tracker);
-								found = true;
-								break;
-							}
-							if (!found)
-							{
-								// Register as calibrated target
-								TargetCalibration3D targetCalib(finaliseTargetMarkers(
-									pipeline.getCalibs(), tgtNew, pipeline.targetCalib.params.post));
-								state.trackerConfigs.emplace_back(tgtNew.targetID, "New Target", std::move(targetCalib));
-								ServerUpdatedTrackerConfig(state, state.trackerConfigs.back());
-							}
+					// Update Targets
+					for (auto &tgtNew : data.targets)
+					{
+						for (auto &tracker : state.trackerConfigs)
+						{ // For now, fetch from trackerConfig - may also fetch from trackedTargets/dormantTargets
+							if (tracker.id != tgtNew.targetID) continue;
+							// Copy and update target calibration
+							TargetCalibration3D calib = tracker.calib;
+							calib.markers = finaliseTargetMarkers(pipeline.getCalibs(), tgtNew, pipeline.targetCalib.params.post);
+							calib.updateMarkers();
+							// Signal server to update calib
+							SignalTargetCalibUpdate(tgtNew.targetID, calib);
+							break;
 						}
 					}
 
@@ -446,11 +432,14 @@ void InterfaceState::UpdatePipeline(InterfaceWindow &window)
 					for (auto &tgtNew : data.targets)
 					{
 						for (auto &tracker : state.trackerConfigs)
-						{
+						{ // For now, fetch from trackerConfig - may also fetch from trackedTargets/dormantTargets
 							if (tracker.id != tgtNew.targetID) continue;
-							updateMarkerOrientations(tracker.calib.markers, calibs, tgtNew, pipeline.targetCalib.params.post);
-							tracker.calib.updateMarkers();
-							ServerUpdatedTrackerConfig(state, tracker);
+							// Copy and update target calibration
+							TargetCalibration3D calib = tracker.calib;
+							updateMarkerOrientations(calib.markers, calibs, tgtNew, pipeline.targetCalib.params.post);
+							calib.updateMarkers();
+							// Signal server to update calib
+							SignalTargetCalibUpdate(tgtNew.targetID, calib);
 							break;
 						}
 					}
@@ -458,14 +447,16 @@ void InterfaceState::UpdatePipeline(InterfaceWindow &window)
 				}, *db_lock);
 			}
 
-			if (ImGui::Button("Save Cameras", SizeWidthDiv3()))
+			if (SaveButton("Save Cameras", SizeWidthDiv3(), state.cameraCalibsDirty))
 			{
-				ServerStoreCameraCalib(state);
+				storeCameraCalibrations("store/camera_calib.json", state.cameraCalibrations);
+				state.cameraCalibsDirty = false;
 			}
 			ImGui::SameLine();
-			if (ImGui::Button("Save Targets", SizeWidthDiv3()))
+			if (SaveButton("Save Targets", SizeWidthDiv3(), state.trackerConfigDirty || state.trackerCalibsDirty || state.trackerIMUsDirty))
 			{
-				ServerStoreTargetConfigs(state);
+				storeTrackerConfigurations("store/trackers.json", state.trackerConfigs);
+				state.trackerConfigDirty = state.trackerCalibsDirty = state.trackerIMUsDirty = false;
 			}
 			ImGui::SameLine();
 			if (ImGui::Button("Clear Database", SizeWidthDiv3()))

server/source/pipeline/pipeline.hpp
@@ -20,6 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #define PIPELINE_H
 
 #include "record.hpp"
+#include "signals.hpp" // Signals to Server
 
 #include "pipeline/parameters.hpp"
 
@@ -286,21 +287,16 @@ void OrphanIMU(PipelineState &pipeline, std::shared_ptr<IMU> &imu);
 /* General functions */
 
 /**
- * Normalise the room (camera calibrations), discarding any room calibration done before
- */
-void NormaliseRoom(PipelineState &pipeline);
-
-/**
  * Calibrate the room coordinate system given a number of floor points and scale for reference (pipeline state)
  */
-bool CalibrateFloor(PipelineState &pipeline);
+bool CalibrateRoom(PipelineState &pipeline);
 
 /**
- * Adopts an existing room calibration into the given new calibrations.
- * Tries to identify at least two cameras that have not changed between the current calibration and this one.
- * If these don't exist, returns false.
+ * Updates pipeline cameras with given calibrations
+ * Optionally copies an existing room calibration from current camera calibrations into the given new calibrations (updating them in the process).
+ * This will try to identify at least two cameras that have not changed between the current calibration and this one.
  */
-bool AdoptRoomCalibration(PipelineState &pipeline, std::vector<CameraCalib> &calibs);
+void AdoptNewCalibrations(PipelineState &pipeline, std::vector<CameraCalib> &calibs, bool copyRoomCalib);
 
 /**
  * Log the parameters and inferred properties of the camera calibrations

server/source/pipeline/pipeline_calibration_point.cpp
@@ -39,6 +39,8 @@ static void ThreadCalibrationOptimisation(std::stop_token stopToken, PipelineSta
 // Calibration
 // ----------------------------------------------------------------------------
 
+static void ApplyTransformation(std::vector<CameraCalib> &calibs, Eigen::Matrix3d orientation, Eigen::Affine3d transform);
+
 void ResetPointCalibration(PipelineState &pipeline)
 {
 	// Wait for all calibration threads to stop if they are still running
@@ -185,6 +187,13 @@ static void ThreadCalibrationReconstruction(std::stop_token stopToken, PipelineS
 		else
 			LOGC(LWarn, "    Room calibration is different from Ground Truth simulation setup! Had error of %.3fmm and %.3fdg\n", errors.first, errors.second);
 	}
+	else
+	{
+		Eigen::Matrix3d roomOrientation;
+		Eigen::Affine3d roomTransform;
+		getCalibNormalisation<double>(calibs, roomOrientation, roomTransform);
+		ApplyTransformation(calibs, roomOrientation, roomTransform);
+	}
 
 	if (stopToken.stop_requested())
 	{
@@ -210,16 +219,11 @@ static void ThreadCalibrationReconstruction(std::stop_token stopToken, PipelineS
 
 	{ // Update calibration
 		std::unique_lock pipeline_lock(pipeline->pipelineLock);
-		for (int c = 0; c < calibs.size(); c++)
-			pipeline->cameras[calibs[c].index]->calib = calibs[c];
-		NormaliseRoom(*pipeline);
+		AdoptNewCalibrations(*pipeline, calibs, true);
 	}
+	SignalCameraCalibUpdate(calibs);
 
-	// Update fundamental matrices using calibration
 	UpdateCalibrationRelations(*pipeline, *pipeline->calibration.contextualLock(), observations);
-
-	SignalServerEvent(EVT_UPDATE_CALIBS);
-	SignalPipelineUpdate();
 
 	LOGC(LInfo, "== Done Reconstructing Calibration!\n");
 
@@ -301,13 +305,9 @@ static void ThreadCalibrationOptimisation(std::stop_token stopToken, PipelineSta
 
 		{ // Update calibration
 			std::unique_lock pipeline_lock(pipeline->pipelineLock);
-			AdoptRoomCalibration(*pipeline, calibs);
-			for (int c = 0; c < calibs.size(); c++)
-				pipeline->cameras[calibs[c].index]->calib = calibs[c];
+			AdoptNewCalibrations(*pipeline, calibs, true);
 		}
-
-		SignalServerEvent(EVT_UPDATE_CALIBS);
-		SignalPipelineUpdate();
+		SignalCameraCalibUpdate(calibs);
 
 		if (errors.max > errors.mean + params.outliers.sigma.trigger*errors.stdDev)
 		{
@@ -378,38 +378,16 @@ static void ThreadCalibrationOptimisation(std::stop_token stopToken, PipelineSta
 
 	{ // Update calibration
 		std::unique_lock pipeline_lock(pipeline->pipelineLock);
-		AdoptRoomCalibration(*pipeline, calibs);
-		for (int c = 0; c < calibs.size(); c++)
-			pipeline->cameras[calibs[c].index]->calib = calibs[c];
+		AdoptNewCalibrations(*pipeline, calibs, true);
 	}
+	SignalCameraCalibUpdate(calibs);
 
-	// Update fundamental matrices using calibration
 	UpdateCalibrationRelations(*pipeline, *pipeline->calibration.contextualLock(), observations);
-
-	SignalServerEvent(EVT_UPDATE_CALIBS);
-	SignalPipelineUpdate();
 
 	LOGC(LDebug, "=======================\n");
 }
 
-void NormaliseRoom(PipelineState &pipeline)
-{
-	Eigen::Matrix3d roomOrientation;
-	Eigen::Affine3d roomTransform;
-	getCalibNormalisation<double>(pipeline.getCalibs(), roomOrientation, roomTransform);
-	for (auto &cam : pipeline.cameras)
-	{
-		if (cam->calib.invalid()) continue;
-		cam->calibBackup = cam->calib;
-		cam->calib.transform.linear() = roomOrientation * cam->calib.transform.linear();
-		cam->calib.transform.translation() = roomTransform * cam->calib.transform.translation();
-		cam->calib.UpdateDerived();
-	}
-	SignalServerEvent(EVT_UPDATE_CALIBS);
-	SignalPipelineUpdate();
-}
-
-bool CalibrateFloor(PipelineState &pipeline)
+bool CalibrateRoom(PipelineState &pipeline)
 {
 	auto &ptCalib = pipeline.pointCalib;
 	if (ptCalib.room.floorPoints.size() < 3)
@@ -434,16 +412,9 @@ bool CalibrateFloor(PipelineState &pipeline)
 		LOG(LPointCalib, LInfo, "Calibrated floor with %d points!\n", pointCount);
 		LOG(LPointCalib, LInfo, "Scaled calibration by %f during floor calibration!",
 			roomTransform.linear().colwise().norm().mean());
-		for (auto &cam : pipeline.cameras)
-		{
-			if (cam->calib.invalid()) continue;
-			cam->calibBackup = cam->calib;
-			cam->calib.transform.linear() = roomOrientation * cam->calib.transform.linear();
-			cam->calib.transform.translation() = roomTransform * cam->calib.transform.translation();
-			cam->calib.UpdateDerived();
-		}
-		SignalServerEvent(EVT_UPDATE_CALIBS);
-		SignalPipelineUpdate();
+		ApplyTransformation(calibs, roomOrientation, roomTransform);
+		AdoptNewCalibrations(pipeline, calibs, false);
+		SignalCameraCalibUpdate(calibs);
 		return true;
 	}
 	else
@@ -453,20 +424,44 @@ bool CalibrateFloor(PipelineState &pipeline)
 	}
 }
 
-bool AdoptRoomCalibration(PipelineState &pipeline, std::vector<CameraCalib> &calibs)
+void AdoptNewCalibrations(PipelineState &pipeline, std::vector<CameraCalib> &calibs, bool copyRoomCalib)
 {
-	std::vector<CameraCalib> oldCalibs(calibs.size());
-	for (int c = 0; c < calibs.size(); c++)
-		oldCalibs[c] = pipeline.cameras[calibs[c].index]->calib;
-	Eigen::Matrix3d roomOrientation;
-	Eigen::Affine3d roomTransform;
-	if (!transferRoomCalibration(oldCalibs, calibs, roomOrientation, roomTransform))
-		return false;
+	if (copyRoomCalib)
+	{
+		std::vector<CameraCalib> oldCalibs(calibs.size());
+		for (int c = 0; c < calibs.size(); c++)
+			oldCalibs[c] = pipeline.cameras[calibs[c].index]->calib;
+		Eigen::Matrix3d roomOrientation;
+		Eigen::Affine3d roomTransform;
+		if (transferRoomCalibration(oldCalibs, calibs, roomOrientation, roomTransform))
+			ApplyTransformation(calibs, roomOrientation, roomTransform);
+	}
+
+	for (auto &cam : pipeline.cameras)
+	{
+		cam->calibBackup = cam->calib; // Create backup
+		cam->calib.id = CAMERA_ID_NONE; // Invalidate existing
+		for (auto &calib : calibs)
+		{ // Try to find valid new calibration
+			if (calib.invalid()) continue;
+			if (calib.id == cam->id)
+			{ // Adopt
+				cam->calib = calib;
+				break;
+			}
+		}
+		// Make sure index is correct
+		cam->calib.index = cam->index;
+	}
+}
+
+static void ApplyTransformation(std::vector<CameraCalib> &calibs, Eigen::Matrix3d orientation, Eigen::Affine3d transform)
+{
 	for (auto &calib : calibs)
 	{
-		calib.transform.linear() = roomOrientation * calib.transform.linear();
-		calib.transform.translation() = roomTransform * calib.transform.translation();
+		if (calib.invalid()) continue;
+		calib.transform.linear() = orientation * calib.transform.linear();
+		calib.transform.translation() = transform * calib.transform.translation();
 		calib.UpdateDerived();
 	}
-	return true;
 }

server/source/pipeline/pipeline_tracking.cpp
@@ -217,7 +217,7 @@ static TrackerRecord &retroactivelyTrackFrame(PipelineState &pipeline, TrackedTa
 		frame->time, frame->num, pipeline.cameras.size(), pipeline.params.track);
 
 	if (result.isTracked() && tracker.inertial)
-		postCorrectIMU(tracker.state, tracker.inertial, tracker.pose, frame->time, pipeline.params.track);
+		postCorrectIMU(tracker, tracker.state, tracker.inertial, tracker.pose, frame->time, pipeline.params.track);
 
 	result.setFlag(TrackingResult::CATCHING_UP);
 	return recordTrackingResult(pipeline, frame, tracker, result);
@@ -396,7 +396,7 @@ void RetroactivelySimulateFilter(PipelineState &pipeline, std::size_t frameStart
 				calibs, points2D, trackRecord, frameRecord.time, frameRecord.num, pipeline.params.track);
 
 			if (trackRecord.result.isTracked() && targetIt->inertial)
-				postCorrectIMU(targetIt->state, targetIt->inertial, targetIt->pose, frameRecord.time, pipeline.params.track);
+				postCorrectIMU(*targetIt, targetIt->state, targetIt->inertial, targetIt->pose, frameRecord.time, pipeline.params.track);
 
 			recordTrackerObservation(trackRecord, targetIt->pose, pipeline.keepInternalData);
 			recordTrackerTarget(trackRecord, targetIt->target, pipeline.keepInternalData);
@@ -488,7 +488,7 @@ void UpdateTrackingPipeline(PipelineState &pipeline, std::vector<CameraPipeline*
 					tracker->id, tracker->label.c_str(), tracker->target.match2D.error.samples, tracker->target.match2D.error.mean*PixelFactor);
 
 				if (tracker->inertial)
-					postCorrectIMU(tracker->state, tracker->inertial, tracker->pose, frame->time, pipeline.params.track);
+					postCorrectIMU(*tracker, tracker->state, tracker->inertial, tracker->pose, frame->time, pipeline.params.track);
 
 				// Occupy all 2D points of tracked target
 				occupyTargetMatches(tracker->target.match2D);

server/source/recording.cpp
@@ -127,18 +127,7 @@ void loadRecording(ServerState &state, Recording &&recordEntries, bool append)
 	{ // Overwrite any calibrations
 		std::vector<CameraCalib> cameraCalibs;
 		parseCameraCalibrations(recordEntries.calib, cameraCalibs);
-
-		for (auto &cam : state.pipeline.cameras)
-		{
-			for (int i = 0; i < cameraCalibs.size(); i++)
-			{
-				if (cameraCalibs[i].id == cam->id)
-				{
-					cam->calib = cameraCalibs[i];
-					cam->calib.index = cam->index;
-					break;
-				}
-			}
-		}
+		std::unique_lock pipeline_lock(state.pipeline.pipelineLock);
+		AdoptNewCalibrations(state.pipeline, cameraCalibs, false);
 	}
 }

server/source/server.cpp
@@ -20,7 +20,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "server.hpp"
 
-#include "ui/shared.hpp" // Signals
+#include "ui/shared.hpp" // Signals to UI
+#include "signals.hpp" // Signals to Server
 
 #include "device/tracking_controller.hpp"
 #include "device/tracking_camera.hpp"
@@ -74,11 +75,16 @@ bool ServerInit(ServerState &state)
 
 	// Read configurations
 	parseGeneralConfigFile("store/general_config.json", state.config);
+	state.generalConfigDirty = false;
+	// TODO: Add UI and storage of general config
 	parseCameraConfigFile("store/camera_config.json", state.cameraConfig);
+	state.cameraConfigDirty = false;
 
 	// Load calibrations
 	parseCameraCalibrations("store/camera_calib.json", state.cameraCalibrations);
+	state.cameraCalibsDirty = false;
 	parseTrackerConfigurations("store/trackers.json", state.trackerConfigs);
+	state.trackerConfigDirty = state.trackerCalibsDirty = state.trackerIMUsDirty = false;
 
 	{
 		// Debug calibration
@@ -117,35 +123,6 @@ void ServerExit(ServerState &state)
 	ResetIO(state);
 }
 
-void ServerStoreConfiguration(ServerState &state)
-{
-	storeCameraConfigFile("store/camera_config.json", state.cameraConfig);
-}
-
-void ServerStoreCameraCalib(ServerState &state)
-{
-	// Update list of calibrations with current set of cameras
-	for (auto &cam : state.pipeline.cameras)
-	{
-		int i;
-		for (i = 0; i < state.cameraCalibrations.size(); i++)
-		{
-			if (state.cameraCalibrations[i].id == cam->id)
-			{ // Found calibration entry, update calibration data
-				state.cameraCalibrations[i] = cam->calib;
-				break;
-			}
-		}
-		if (i >= state.cameraCalibrations.size())
-		{ // New camera with calibration, add as calibration entry
-			state.cameraCalibrations.push_back(cam->calib);
-		}
-	}
-
-	// Write both as current calibration
-	storeCameraCalibrations("store/camera_calib.json", state.cameraCalibrations);
-}
-
 void ServerUpdatedTrackerConfig(ServerState &state, TrackerConfig &tracker)
 {
 	if (state.isStreaming)
@@ -170,32 +147,63 @@ void ServerUpdatedTrackerConfig(ServerState &state, TrackerConfig &tracker)
 	}
 }
 
-void ServerStoreTargetConfigs(ServerState &state)
-{
-	storeTrackerConfigurations("store/trackers.json", state.trackerConfigs);
-}
+// Signals
 
-void ServerUpdateTrackerTargetCalib(ServerState &state, int trackerID, TargetCalibration3D calib)
+void SignalTargetCalibUpdate(int trackerID, TargetCalibration3D calib)
 {
-	for (auto &tracker : state.trackerConfigs)
+	for (auto &tracker : GetState().trackerConfigs)
 	{
 		if (tracker.id != trackerID) continue;
-		tracker.calib = std::move(calib);
-		// Assuming here that if this function was called, the calib was already updated in the TrackedTarget
+		tracker.calib = calib;
+		if (tracker.isSimulated)
+		{ // Ensure calibrated, simulated targets are adopted as normal targets and stored
+			tracker.isSimulated = false;
+		}
+		GetState().trackerCalibsDirty = true;
+		// Update pipeline - this update may have come from pipeline, still
+		ServerUpdatedTrackerConfig(GetState(), tracker);
 		return;
 	}
 }
 
-void ServerUpdateTrackerIMUCalib(ServerState &state, int trackerID, IMUIdent imu, IMUCalib calib)
+void SignalIMUCalibUpdate(int trackerID, IMUIdent ident, IMUCalib calib)
 {
-	for (auto &tracker : state.trackerConfigs)
+	for (auto &tracker : GetState().trackerConfigs)
 	{
 		if (tracker.id != trackerID) continue;
-		tracker.imuIdent = imu;
+		tracker.imuIdent = ident;
 		tracker.imuCalib = calib;
-		// Assuming here that if this function was called, the IMU was already connected to the tracker
+		GetState().trackerIMUsDirty = true;
+		// Update pipeline - this update may have come from pipeline, still
+		ServerUpdatedTrackerConfig(GetState(), tracker);
 		return;
 	}
+}
+
+void SignalCameraCalibUpdate(std::vector<CameraCalib> calibs)
+{
+	auto &state = GetState();
+	for (auto &calib : calibs)
+	{ // Integrate changed calibs into stored list of calibrations
+		if (calib.invalid()) continue;
+		int i;
+		for (i = 0; i < state.cameraCalibrations.size(); i++)
+		{
+			if (state.cameraCalibrations[i].id == calib.id)
+			{ // Found calibration entry, update calibration data
+				state.cameraCalibrations[i] = calib;
+				break;
+			}
+		}
+		if (i >= state.cameraCalibrations.size())
+		{ // New camera with calibration, add as calibration entry
+			state.cameraCalibrations.push_back(calib);
+		}
+	}
+	GetState().cameraCalibsDirty = true;
+	// Signal UI that calibrations got updated
+	SignalServerEvent(EVT_UPDATE_CALIBS);
+	// This update is assumed to have come from pipeline, so no need to update it
 }
 
 // ----------------------------------------------------------------------------
@@ -570,8 +578,6 @@ static void DeviceSupervisorThread(std::stop_token stop_token, ServerState *stat
 			IMUDeviceList removedIMUs, addedIMUs;
 			for (auto imuProvider = imuLock->begin(); imuProvider != imuLock->end();)
 			{
-				assert(*imuProvider);
-				// TODO: Fix seg fault here after re-entering device mode once
 				auto status = imuProvider->get()->poll(updatedDevices, removedIMUs, addedIMUs);
 				if (status == IMU_STATUS_DISCONNECTED)
 				{
@@ -582,10 +588,9 @@ static void DeviceSupervisorThread(std::stop_token stop_token, ServerState *stat
 					imusRegistered = true;
 				imuProvider++;
 			}
-			if (updatedDevices > 0)
-				SignalPipelineUpdate();
 			if (!addedIMUs.empty() || !removedIMUs.empty())
 			{
+				SignalPipelineUpdate();
 				std::unique_lock pipeline_lock(state->pipeline.pipelineLock);
 				for (auto &imuDevice : addedIMUs)
 				{
@@ -753,13 +758,9 @@ void StartSimulation(ServerState &state)
 	}
 
 	{ // Align calibrations to simulated calibrations
-		std::vector<CameraCalib> calibs;
-		for (auto &cam : state.pipeline.cameras)
-			calibs.push_back(cam->calib);
+		auto calibs = state.pipeline.getCalibs();
 		AlignWithGT(state.pipeline, calibs);
-		for (int c = 0; c < calibs.size(); c++)
-			state.pipeline.cameras[c]->calib = calibs[c];
-		SignalServerEvent(EVT_UPDATE_CALIBS);
+		AdoptNewCalibrations(state.pipeline, calibs, false);
 	}
 
 	// Debug calibrations
@@ -835,7 +836,7 @@ static void SimulationThread(std::stop_token stop_token, ServerState *state)
 		}
 
 		if (!state->isStreaming || stop_token.stop_requested())
-			break;
+			continue;
 
 		// Check after breakpoint to allow for halting while in breakpoint
 		if (state->simAdvance == 0)
@@ -847,7 +848,7 @@ static void SimulationThread(std::stop_token stop_token, ServerState *state)
 		}
 
 		if (!state->isStreaming || stop_token.stop_requested())
-			break;
+			continue;
 
 		long desiredFrameIntervalUS = 1000000 / state->controllerConfig.framerate;
 
@@ -971,7 +972,7 @@ static void SimulationThread(std::stop_token stop_token, ServerState *state)
 		pipeline_lock.unlock();
 
 		if (!state->isStreaming || stop_token.stop_requested())
-			break;
+			continue;
 
 		auto frameReceiveTime = sclock::now();
 
@@ -1172,9 +1173,6 @@ void StopStreaming(ServerState &state)
 	SignalServerEvent(EVT_STOP_STREAMING);
 
 	LOG(LDefault, LInfo, "Stopped stream!\n");
-
-	// Store any target or IMU calibration changes that happened during this session
-	ServerStoreTargetConfigs(state);
 }
 
 void ProcessStreamFrame(SyncGroup &sync, SyncedFrame &frame, bool premature)

commit 0b060b0ba2cccca96dc2e1f2bd8c562734cbd257

Fixed calibration regression Was introduced in 949e15948d557e57cea69d0d1ca318712589da05 Fixed cause and guarded against symptoms of bug

server/source/calib/optimisation.cpp
@@ -216,6 +216,7 @@ OptErrorRes updateCameraErrorMaps(const ObsData &data, const std::vector<CameraC
 	{
 		LOGC(LWarn, "Error Vector has NaNs!\n");
 		LOGC(LWarn, "Error Vector size: %d, tri point count: %d!\n", (int)errorVec.size(), (int)errorTerm.m_data->points.points.size());
+		return {};
 	}
 
 	// Calculate outlier limit

server/source/calib_point/calibration_room.cpp
@@ -110,6 +110,11 @@ OptErrorRes determinePointOutliers(ObsData &data, const std::vector<CameraCalib>
 		} */
 		for (auto &sample : point.samples)
 		{
+			if (std::isnan(errorVec(index)))
+			{
+				index++;
+				continue;
+			}
 			cameraTotalCounts[sample.camera]++;
 			cameraErrorsTotal(sample.camera) += errorVec(index)*errorVec(index);
 			// Update bucket
@@ -162,6 +167,8 @@ OptErrorRes determinePointOutliers(ObsData &data, const std::vector<CameraCalib>
 		{
 			auto &sample = point.samples[s];
 			Scalar error = errorVec(index + s);
+			if (std::isnan(error))
+				isOutlierForce = true;
 			if (error > outlierLimit)
 				isOutlier = true;
 			if (error > outlierLimitForce)
@@ -382,6 +389,8 @@ bool transferRoomCalibration(const std::vector<CameraCalib_t<Scalar>> &calibsSrc
 	int unchangedCameras = 0;
 	for (int c = 0; c < calibsSrc.size(); c++)
 	{
+		if (!calibsSrc[c].valid())
+			continue;
 		// Guess scale by relation to other cameras
 		std::vector<Eigen::Vector<Scalar,1>> scaleGuesses;
 		for (int cc = 0; cc < calibsSrc.size(); cc++)
@@ -397,7 +406,10 @@ bool transferRoomCalibration(const std::vector<CameraCalib_t<Scalar>> &calibsSrc
 			float scale = 0.0f;
 			for (int i : scaleGroups.front())
 				scale += scaleGuesses[i](0);
-			scaleGuesses = { Eigen::Vector<Scalar,1>(scale/scaleGroups.front().size()) };
+			scale /= scaleGroups.front().size();
+			if (scale < 0.001f || scale > 10000.0f)
+				continue;
+			scaleGuesses = { Eigen::Vector<Scalar,1>(scale) };
 		}
 		LOG(LPointCalib, LTrace, "Finding corrective transform for camera %d, %d scale guesses (after %d groups)",
 			c, (int)scaleGuesses.size(), (int)scaleGroups.size());

server/source/pipeline/pipeline.cpp
@@ -376,6 +376,13 @@ static void UpdateCalibrationRelation(CameraSystemCalibration &calibration, cons
 	// Determine fundamental matrix from calibration
 	FundamentalMatrix FM = {};
 	FM.matrix = calculateFundamentalMatrix<float>(calibA, calibB);
+	Eigen::JacobiSVD<Eigen::Matrix3f> svd(FM.matrix);
+	if (svd.rank() < 2)
+	{ // Not a valid FM
+		LOG(LPipeline, LWarn, "Existing camera relation %d-%d is invalid with rank %d < 2! Discarding!", calibA.index, calibB.index, (int)svd.rank());
+		return;
+	}
+
 	FM.precalculated = true;
 
 	// Calculate full error stats

server/source/point/sequences2D.cpp
@@ -100,8 +100,17 @@ static inline StatDistf calculateCorrespondenceStats(const Range<VecIt> &rangesA
 	for (auto itA = rangesA.start, itB = rangesB.start; itA != rangesA.end && itB != rangesB.end; itA++, itB++)
 	{
 		float error = itB->homogeneous().transpose() * FM * itA->homogeneous();
-		assert(!std::isnan(error));
+		if (std::isnan(error) || std::isinf(error))
+		{
+			LOG(LSequence, LWarn, "Had NAN/INF error during correspondence check!");
+			return {};
+		}
 		errorStats.update(std::abs(error));
+	}
+	if (errorStats.num > 0 && errorStats.avg == 0)
+	{
+		LOG(LSequence, LWarn, "Had 0 total error during correspondence check!");
+		return {};
 	}
 	return errorStats;
 }
@@ -254,7 +263,8 @@ static int resolveCorrespondences(const SequenceAquisitionParameters &params, Ca
 			// Calculate new fundamental matrix from sequence overlap
 			FundamentalMatrix FMseq = {};
 			float confidence = calculateFundamentalMatrix<float>(FMseq, rangesA, rangesB);
-			assert(FMseq.stats.num == overlap.length);
+			if (FMseq.stats.num < params.correspondences.minOverlap)
+				continue;
 
 			// Check for basic correspondence
 			if (confidence < params.correspondences.minConfidence)
@@ -316,6 +326,7 @@ static int resolveCorrespondences(const SequenceAquisitionParameters &params, Ca
 			errorAvg += corr.stats.avg;
 			assert(!std::isnan(errorAvg));
 			assert(!std::isnan(correspondingWeight) && !std::isnan(discreditingWeight));
+			assert(!std::isinf(correspondingWeight) && !std::isinf(discreditingWeight));
 		}
 		errorAvg /= marker.cameras.size();
 
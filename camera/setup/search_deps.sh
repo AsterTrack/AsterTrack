2eb8a0531f047af7d488477d08ba4d2963a5c611
Seneral <seneral@seneral.dev>
2025-12-20 13:23:37 +0100

    Improved setup tooling

--- camera/setup/search_deps.sh
+++ camera/setup/search_deps.sh
@@ -5,8 +5,8 @@ REPO_ID=$2
 DOWNLOAD_PATH=$3
 
 if [[ $SEARCH_QUERY = "help" || $SEARCH_QUERY = "h" ]]; then
-	echo "Usage: sudo search_packages.sh SEARCH_QUERY REPO_ID [DOWNLOAD_PATH]
-	SEARCH_QUERY: grep formatted term to search for in packages filetree
+	echo "Usage: sudo search_deps.sh SEARCH_QUERY REPO_ID [DOWNLOAD_PATH]
+	SEARCH_QUERY: grep formatted term to search for in packages dependencies
 	REPO_ID: id of repository, e.g. '13.x/armv6' from http://www.tinycorelinux.net/13.x/armv6/tcz
 	DOWNLOAD_PATH: folder to download packages to"
 	exit 0
@@ -29,20 +29,17 @@ fi
 PACKAGES="$PACKAGES $(cat "repo.txt")"
 for pkg in $PACKAGES; do
 
-	# Download package lists if not already there
-	if [[ ! -f $pkg.list ]]; then
-		url="$REPO_URL/$pkg.list"
+	# Download package deps if not already there
+	if [[ ! -f $pkg.dep ]]; then
+		url="$REPO_URL/$pkg.dep"
 		wget -N -q --show-progress $url
 	fi
-	if [[ ! -f $pkg.list ]]; then
-		echo "Failed to download $pkg list from the repo ($url)!"
-		exit 1
-	fi
-
-	query=$(grep $SEARCH_QUERY $pkg.list)
-	if [[ "$query" != "" ]]; then
-		echo "Found results in package $pkg (in $REPO_ID repository):"
-		echo $query
+	if [[ -f "$pkg.dep" ]]; then
+		query=$(grep $SEARCH_QUERY $pkg.dep)
+		if [[ "$query" != "" ]]; then
+			echo "Found results in package $pkg (in $REPO_ID repository):"
+			echo $query
+		fi
 	fi
 done
 

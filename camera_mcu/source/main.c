AsterTrack/AsterTrack

commit ba6b9a355528609ec91bd9fad422bf951876dbe9

Allow I2C to respond with DMA from memory Default 256 byte response buffer for most packets But DMA large data like OTP String (or future forwarded UART packets)

camera_mcu/source/main.c
@@ -371,7 +371,7 @@ int main(void)
 		if (piIsBooted && GetTimeSpanMS(lastPiComm, now) > MCU_COMM_TIMEOUT_MS)
 			piIsBooted = false;
 		if (uartState == UART_CamPi && !piIsBooted)
-		{ // Pi stopped corresponding, clear booted flag and take back UART control
+		{ // Pi stopped corresponding, take back UART control
 			EnterUARTPortZone(0);
 			uartState = UART_None;
 
@@ -751,10 +751,12 @@ static uint16_t fillInfoPacket(uint8_t *response, uint8_t requestedVersion)
 }
 
 
-uint8_t i2cd_prepare_response(enum CameraMCUCommand command, uint8_t *data, uint8_t len, uint8_t response[256])
+uint8_t i2cd_prepare_response(enum CameraMCUCommand command, uint8_t *data, uint8_t len, uint8_t **responsePtr)
 {
 	lastPiComm = GetTimePoint();
 	piIsBooted = true;
+	uint8_t *response = *responsePtr; // Maximum size: I2C_TRANSMIT_BUFFER_LEN
+	// WARNING: If responsePtr is changed to an external buffer, it needs to have space for I2C_PREPENDED_BYTES in front of it!
 
 	switch (command)
 	{
@@ -800,15 +802,18 @@ uint8_t i2cd_prepare_response(enum CameraMCUCommand command, uint8_t *data, uint
 		case MCU_GET_HW_STR:
 		{
 			// We assume OTP has already been read - otherwise, the Pi would not know the length to read
-			response[0] = OTP_HwStringLength >> 8;
-			response[1] = OTP_HwStringLength & 0xFF;
-			memcpy(response+2, OTP_HwStringData, OTP_HwStringLength);
+			// And it's more complex to read anyway. Advantage: We can just DMA from memory
+			uint16_t pos = OTP_HW_STRING_PREPEND-2;
+			response[pos+0] = OTP_HwStringLength >> 8;
+			response[pos+1] = OTP_HwStringLength & 0xFF;
+			*responsePtr = OTP_HwStringData+pos;
 			return 2 + OTP_HwStringLength;
 		}
 		case MCU_GET_FW_STR:
 		{
 			response[0] = firmwareDescriptorLength >> 8;
 			response[1] = firmwareDescriptorLength & 0xFF;
+			// Can't DMA from rodata (flash)
 			memcpy(response+2, firmwareDescriptor, firmwareDescriptorLength);
 			return 2 + firmwareDescriptorLength;
 		}

camera_mcu/source/i2c_driver.h
@@ -29,12 +29,18 @@ extern "C"
 #include <stdint.h>
 #include <stdbool.h>
 
+// Prepended bytes in addition to the first leading byte
+// See comments on MCU_LEADING_BYTES or I2C_USE_CLOCK_STRETCHING
+#define I2C_PREPENDED_BYTES			(MCU_LEADING_BYTES > 1? (MCU_LEADING_BYTES-1) : 0)
+
+#define I2C_RECEIVE_BUFFER_LEN      256
+#define I2C_TRANSMIT_BUFFER_LEN     256
 
 /* Structures */
 
 // Implemented generally
 bool i2cd_handle_command(enum CameraMCUCommand command, uint8_t *data, uint8_t len);
-uint8_t i2cd_prepare_response(enum CameraMCUCommand command, uint8_t *data, uint8_t len, uint8_t response[256]);
+uint8_t i2cd_prepare_response(enum CameraMCUCommand command, uint8_t *data, uint8_t len, uint8_t **responsePtr);
 
 // Implemented specfically
 void i2c_driver_init();

camera_mcu/source/otp.c
@@ -29,7 +29,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 uint8_t OTP_Version; // Assume OTP not written, disable parsing
 uint8_t OTP_LenMainData, OTP_MaxSubParts, OTP_MaxHWString; // Lengths in 64Bit blocks
-uint8_t OTP_HwStringData[100*8]; // Ensure >= OTP_MaxHWString
+#define MAX_HW_STRING_BLOCKS 100
+uint8_t OTP_HwStringData[OTP_HW_STRING_PREPEND+MAX_HW_STRING_BLOCKS*8];
 uint16_t OTP_HwStringLength;
 uint8_t OTP_NumSubParts;
 
@@ -48,7 +49,7 @@ void otp_read()
 		case 1:
 			OTP_LenMainData = 8;	// Header, HW Serial, 5 Blocks Reserved
 			OTP_MaxSubParts = 8;	// 8 64-Bit Serial Numbers for sub-parts
-			OTP_MaxHWString = 100;	// 100 Blocks maximum for HW Descriptor String
+			OTP_MaxHWString = MAX_HW_STRING_BLOCKS;	// 100 Blocks maximum for HW Descriptor String
 			break;
 		default: // Unsupported, should not happen, assume 0 sizes and don't parse
 			OTP_LenMainData = 0;
@@ -56,7 +57,7 @@ void otp_read()
 			OTP_MaxHWString = 0;
 			break;
 	}
-	// assert(sizeof(OTP_HwStringData) >= OTP_MaxHWString*8);
+	// assert(sizeof(OTP_HwStringData) >= OTP_HW_STRING_PREPEND+OTP_MaxHWString*8);
 
 	if (OTP_MaxHWString > 0)
 	{
@@ -67,6 +68,7 @@ void otp_read()
 		// Upon encountering a block that has not yet been written, string is terminated again
 		uint8_t otpTextStart = (OTP_LenMainData + OTP_MaxSubParts) * 2;
 		OTP_HwStringLength = 0;
+		uint8_t *OTP_HWStringPtr = OTP_HwStringData + OTP_HW_STRING_PREPEND;
 		for (int b = 0; b < OTP_MaxHWString; b++)
 		{
 			uint8_t otpPtr32 = otpTextStart + b*2;
@@ -78,15 +80,15 @@ void otp_read()
 			{
 				if ((block & 0xFF) == 0)
 				{ // Insert text separator
-					OTP_HwStringData[OTP_HwStringLength++] = MCU_MULTI_TEXT_SEP;
+					OTP_HWStringPtr[OTP_HwStringLength++] = MCU_MULTI_TEXT_SEP;
 					break;
 				}
-				OTP_HwStringData[OTP_HwStringLength++] = block & 0xFF;
+				OTP_HWStringPtr[OTP_HwStringLength++] = block & 0xFF;
 				block >>= 8;
 			}
 		}
 		// Clear trailing separator - but retain the separators between individual texts
-		if (OTP_HwStringLength > 0 && OTP_HwStringLength < OTP_MaxHWString*8 && OTP_HwStringData[OTP_HwStringLength-1] == MCU_MULTI_TEXT_SEP)
+		if (OTP_HwStringLength > 0 && OTP_HwStringLength < OTP_MaxHWString*8 && OTP_HWStringPtr[OTP_HwStringLength-1] == MCU_MULTI_TEXT_SEP)
 			OTP_HwStringLength--;
 	}
 	else OTP_HwStringLength = 0;

camera_mcu/source/otp.h
@@ -22,7 +22,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <stdint.h>
 
 extern uint8_t OTP_Version;
-extern uint8_t OTP_HwStringData[];
+#define OTP_HW_STRING_PREPEND   8   // For I2C_PREPENDED_BYTES + 16bit size
+extern uint8_t OTP_HwStringData[];  // OTP_HW_STRING_PREPEND + actual string size
 extern uint16_t OTP_HwStringLength;
 extern uint8_t OTP_NumSubParts;
 

camera_mcu/targets/ARM/source/i2c_arm_ll.c
@@ -49,10 +49,9 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // But perhaps keeping the leading bytes just in case to prevent any protocol changes would be good... 
 // NOSTRECH with no leading bytes will NOT work - ZERO reaction time for ISR to parse RX and prepare TX
 #define I2C_USE_CLOCK_STRETCHING	(true || MCU_LEADING_BYTES == 0)
-#define I2C_PREPENDED_BYTES			(MCU_LEADING_BYTES > 1? (MCU_LEADING_BYTES-1) : 0) // Prepended bytes in addition to the first leading byte
 
-static uint8_t receiveBuffer[256];
-static uint8_t transmitBuffer[256];
+static uint8_t receiveBuffer[I2C_RECEIVE_BUFFER_LEN];
+static uint8_t transmitBuffer[I2C_PREPENDED_BYTES+I2C_TRANSMIT_BUFFER_LEN];
 
 
 /* Driver Functions */
@@ -182,25 +181,31 @@ void I2C1_IRQHandler(void)
 				TimePoint start = GetTimePoint();
 
 				// Handle RX data to determine TX data
-				uint8_t transmitLength = 0;
+				uint16_t transmitLength = 0;
+				uint8_t *transmitPtr = transmitBuffer+I2C_PREPENDED_BYTES;
 				if (LL_DMA_IsEnabledChannel(DMA1, DMA_CH_RX) && GetRXLength() > 0)
-					transmitLength = i2cd_prepare_response((enum CameraMCUCommand)receiveBuffer[0], receiveBuffer+1, GetRXLength()-1, transmitBuffer+I2C_PREPENDED_BYTES);
+					transmitLength = i2cd_prepare_response((enum CameraMCUCommand)receiveBuffer[0], receiveBuffer+1, GetRXLength()-1, &transmitPtr);
 				LL_DMA_DisableChannel(DMA1, DMA_CH_RX);
 
-				// Prepare TX data with further leading bytes and a trailing byte which will reset TXDR to 0 after expected transmit length has been reached
-				for (int i = 0; i < I2C_PREPENDED_BYTES; i++)
-					transmitBuffer[i] = 0;
-				transmitBuffer[transmitLength] = 0;
-
-				if (MCU_LEADING_BYTES == 0)
+				bool externalBuffer = transmitPtr != transmitBuffer+I2C_PREPENDED_BYTES;
+				if ((!externalBuffer && transmitLength > I2C_TRANSMIT_BUFFER_LEN) || transmitLength == 0)
+				{ // Don't send anything
+					I2C1->TXDR = transmitPtr[0];
+				}
+				else if (MCU_LEADING_BYTES == 0)
 				{ // Prepare first byte and setup to DMA the rest
-					I2C1->TXDR = transmitBuffer[0];
-					LL_DMA_SetMemoryAddress(DMA1, DMA_CH_TX, (uint32_t)transmitBuffer+1);
-					LL_DMA_SetDataLength(DMA1, DMA_CH_TX, I2C_PREPENDED_BYTES+transmitLength-1+1);
+					I2C1->TXDR = transmitPtr[0];
+					LL_DMA_SetMemoryAddress(DMA1, DMA_CH_TX, (uint32_t)transmitPtr+1);
+					LL_DMA_SetDataLength(DMA1, DMA_CH_TX, transmitLength-1);
 				}
 				else
 				{ // First leading byte is already in TXDR, setup to DMA the rest
-					LL_DMA_SetMemoryAddress(DMA1, DMA_CH_TX, (uint32_t)transmitBuffer);
+					// Prepare TX data with further leading bytes and a trailing byte which will reset TXDR to 0 after expected transmit length has been reachedif (transmitPtr)
+					transmitPtr[transmitLength] = 0; // All external buffers have to allow this
+					transmitPtr -= I2C_PREPENDED_BYTES; // All external buffers have to account for this
+					for (int i = 0; i < I2C_PREPENDED_BYTES; i++)
+						transmitPtr[i] = 0;
+					LL_DMA_SetMemoryAddress(DMA1, DMA_CH_TX, (uint32_t)transmitPtr);
 					LL_DMA_SetDataLength(DMA1, DMA_CH_TX, I2C_PREPENDED_BYTES+transmitLength+1);
 				}
 				LL_DMA_EnableChannel(DMA1, DMA_CH_TX);

commit 56045904d50b240fda097dfdb998ad3bf39e509c

Fixed regression in HW String reporting Introduced in ba6b9a3

camera_mcu/source/main.c
@@ -843,9 +843,10 @@ uint8_t i2cd_prepare_response(enum CameraMCUCommand command, uint8_t *data, uint
 			// We assume OTP has already been read - otherwise, the Pi would not know the length to read
 			// And it's more complex to read anyway. Advantage: We can just DMA from memory
 			uint16_t pos = OTP_HW_STRING_PREPEND-2;
+			*responsePtr = OTP_HwStringData+pos;
+			response = *responsePtr;
 			response[pos+0] = OTP_HwStringLength >> 8;
 			response[pos+1] = OTP_HwStringLength & 0xFF;
-			*responsePtr = OTP_HwStringData+pos;
 			return 2 + OTP_HwStringLength;
 		}
 		case MCU_GET_FW_STR:
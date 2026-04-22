AsterTrack/AsterTrack

commit 8dc00d039cf843972488e4e7ee57a00bf448614f

Fixed Zero-Length UART packet not being handled properly ZLPs did not get a final packet call in UART handling This resulted in ZLP (e.g. request info) from clogging UART packet queue Also fixed ZLP checksum calculation (they don't have any)

shared/mcu/uartd.c
@@ -85,7 +85,7 @@ inline void LeaveUARTPortZone(uint8_t port)
 
 uartd_respond uartd_handle_header(uint_fast8_t port);
 uartd_respond uartd_handle_data(uint_fast8_t port, uint8_t* ptr, uint_fast16_t size);
-uartd_respond uartd_handle_packet(uint_fast8_t port, uint_fast16_t endPos);
+uartd_respond uartd_handle_packet(uint_fast8_t port);
 void uartd_handle_reset(uint_fast8_t port);
 
 
@@ -232,7 +232,7 @@ static uartd_respond uartd_process_data(uint_fast8_t port, uint_fast16_t begin, 
 			state->dataPos += blockSize;
 			if (complete && !state->ignoreData)
 			{
-				resp = uartd_handle_packet(port, pos);
+				resp = uartd_handle_packet(port);
 				if (resp == uartd_reset || resp == uartd_reset_nak)
 				{
 					WARN_STR("!UartPacketBad");
@@ -307,7 +307,7 @@ static uartd_respond uartd_process_data(uint_fast8_t port, uint_fast16_t begin, 
 					if (state->header.length > 0)
 						state->inData = true;
 					else if (!state->ignoreData) // Handle empty packets
-						uartd_handle_data(port, state->bufferPtr+pos, 0);
+						uartd_handle_packet(port);
 				}
 			}
 			TimeSpan dT = GetTimeSinceUS(now);

camera_mcu/source/main.c
@@ -526,6 +526,11 @@ uartd_respond uartd_handle_header(uint_fast8_t port)
 		{ // Firmware blocks have extensive redundancy and resend mechanisms by design, AND they are too large to handle by MCU
 			return uartd_ignore;
 		}
+		if (HasSBCQueuePackets() && !SBCPacketQueue[SBCQueueHead].available)
+		{ // Should not happen, imples UART parsing failed to call final packet
+			BREAK();
+			SBCPacketQueue[SBCQueueHead].available = true;
+		}
 		int8_t packet = AllocateSBCPacket(PACKET_HEADER_SIZE+state->header.length+PACKET_CHECKSUM_SIZE);
 		if (packet < 0)
 		{ // Queue full, may theoretically happen, but should not
@@ -540,7 +545,7 @@ uartd_respond uartd_handle_header(uint_fast8_t port)
 	}
 	else
 	{ // Other protocol comms with SBC (Idents, NAKs, etc)
-		return uartd_accept;
+		return uartd_ignore;
 	}
 }
 
@@ -567,7 +572,7 @@ uartd_respond uartd_handle_data(uint_fast8_t port, uint8_t* ptr, uint_fast16_t s
 	return uartd_accept;
 }
 
-uartd_respond uartd_handle_packet(uint_fast8_t port, uint_fast16_t endPos)
+uartd_respond uartd_handle_packet(uint_fast8_t port)
 {
 	PortState *state = &portStates[port];
 
@@ -580,21 +585,23 @@ uartd_respond uartd_handle_packet(uint_fast8_t port, uint_fast16_t endPos)
 		return uartd_accept;
 	}
 
-	// Verify direct checksum
-	receive.packetSize -= PACKET_CHECKSUM_SIZE;
-	uint8_t checksum[PACKET_CHECKSUM_SIZE];
-	calculateDirectPacketChecksum(receive.packetBuffer, receive.packetSize, checksum);
 	bool correctChecksum = true;
-	for (int i = 0; i < PACKET_CHECKSUM_SIZE; i++)
-	{
-		if (checksum[i] != receive.packetBuffer[receive.packetSize+i])
+	if (receive.packetSize >= PACKET_CHECKSUM_SIZE)
+	{ // Verify direct checksum
+		receive.packetSize -= PACKET_CHECKSUM_SIZE;
+		uint8_t checksum[PACKET_CHECKSUM_SIZE];
+		calculateDirectPacketChecksum(receive.packetBuffer, receive.packetSize, checksum);
+		for (int i = 0; i < PACKET_CHECKSUM_SIZE; i++)
 		{
-			WARN_STR("!PacketChecksum:");
-			WARN_CHARR(INT9_TO_CHARR(port), '+', UI8_TO_HEX_ARR(state->header.tag), '+', INT999_TO_CHARR(state->header.length));
-			correctChecksum = false;
-			rgbled_transition(LED_UART_ERROR, 0);
-			ReturnToDefaultLEDState(UART_RESET_TIMEOUT_MS);
-			break;
+			if (checksum[i] != receive.packetBuffer[receive.packetSize+i])
+			{
+				WARN_STR("!PacketChecksum:");
+				WARN_CHARR(INT9_TO_CHARR(port), '+', UI8_TO_HEX_ARR(state->header.tag), '+', INT999_TO_CHARR(state->header.length));
+				correctChecksum = false;
+				rgbled_transition(LED_UART_ERROR, 0);
+				ReturnToDefaultLEDState(UART_RESET_TIMEOUT_MS);
+				break;
+			}
 		}
 	}
 
@@ -863,9 +870,17 @@ uint8_t i2cd_prepare_response(enum CameraMCUCommand command, uint8_t *data, uint
 		}
 		case MCU_GET_PACKET:
 		{
-			if (!HasSBCQueuePackets() || !SBCPacketQueue[SBCQueueHead].available)
+			if (!HasSBCQueuePackets())
 			{ // SBC should not query without checking status first
 				return 0;
+			}
+			if (!SBCPacketQueue[SBCQueueHead].available)
+			{ // SBC should not query without checking status first
+				if (GetSBCQueueSize() == 1)
+					return 0;
+				// Otherwise, something went terribly wrong (should not happen)
+				BREAK();
+				SBCPacketQueue[SBCQueueHead].available = true;
 			}
 			uint8_t packet = SBCQueueHead;
 			SBCQueueHead = (SBCQueueHead+1) % SBCQueueSize;

camera/source/mcu/mcu.cpp
@@ -802,17 +802,19 @@ bool mcu_fetch_packet(uint16_t size)
 		return false;
 	}
 
-	uint8_t checksum[PACKET_CHECKSUM_SIZE];
-	calculateForwardPacketChecksum(packet, header.length, checksum);
 	bool correctChecksum = true;
-	for (int i = 0; i < PACKET_CHECKSUM_SIZE; i++)
+	if (header.length > 0)
 	{
-		if (checksum[i] != packet[header.length+i])
-			correctChecksum = false;
+		uint8_t checksum[PACKET_CHECKSUM_SIZE];
+		calculateForwardPacketChecksum(packet, header.length, checksum);
+		for (int i = 0; i < PACKET_CHECKSUM_SIZE; i++)
+		{
+			if (checksum[i] != packet[header.length+i])
+				correctChecksum = false;
+		}
+		if (!correctChecksum)
+			printf("Failed to verify packet checksum from MCU with tag %d and size %d!\n", header.tag, header.length);
 	}
-
-	if (!correctChecksum)
-		printf("Failed to verify packet checksum from MCU with tag %d and size %d!\n", header.tag, header.length);
 
 	return ReceivePacketData(state, *comms.get(COMM_MEDIUM_UART), header, packet, header.length, !correctChecksum);
 }

commit 35d0953e7b8ac916cc8a8fc678c79462a4330476

Fixed regression with ZLP parsing Introduced in 8dc00d0

controller/source/main.c
@@ -1491,9 +1491,10 @@ uartd_respond uartd_handle_data(uint_fast8_t port, uint8_t* ptr, uint_fast16_t s
 			state->queuedPos = (ptr-state->bufferPtr) - PACKET_HEADER_SIZE;
 			memcpy(state->bufferPtr+state->queuedPos, state->headerRaw, PACKET_HEADER_SIZE);
 		}
+		//assert(state->header.length > 0);
 		uint_fast16_t end = (ptr-state->bufferPtr) + size;
 		uint16_t remSize = end - state->queuedPos; // Size remaining to be sent
-		uint16_t expSize = state->header.length > 0? state->header.length+PACKET_CHECKSUM_SIZE : 0;
+		uint16_t expSize = state->header.length+PACKET_CHECKSUM_SIZE;
 		bool completed = state->dataPos+size == expSize;
 		bool endOfBuf = end == UART_RX_BUFFER_SIZE;
 		{ // Temp debug
@@ -1515,12 +1516,6 @@ uartd_respond uartd_handle_data(uint_fast8_t port, uint8_t* ptr, uint_fast16_t s
 				return uartd_ignore;
 			}
 			state->queuedPos += handled;
-		}
-
-		if (endOfBuf)
-		{
-			state->queuedPos = 0;
-			//TEMP_CHARR('+', 'R', 'S', 'T', 'Q');
 		}
 
 		TimeSpan dT = GetTimeSinceUS(now);
@@ -1546,14 +1541,24 @@ uartd_respond uartd_handle_data(uint_fast8_t port, uint8_t* ptr, uint_fast16_t s
 	}
 }
 
-uartd_respond uartd_handle_packet(uint_fast8_t port, uint_fast16_t endPos)
+uartd_respond uartd_handle_packet(uint_fast8_t port)
 {
 	PortState *state = &portStates[port];
 	CameraState *cam = &camStates[port];
 	TimePoint now = GetTimePoint();
 
 	if (state->header.tag >= PACKET_HOST_COMM)
-	{ // Already sent all data to host
+	{ 
+		if (state->header.length == 0)
+		{ // Header was not sent yet
+			uint16_t remSize = PACKET_HEADER_SIZE;
+			handleSourceData(&packetHub, &packetHub.sources[port], state->headerRaw, &remSize);
+			if (remSize != 0)
+				ERR_STR("!OVF_ZLP");
+		}
+
+		// Sent all data to host
+		state->queuedPos = 0;
 		return uartd_accept;
 	}
 

shared/mcu/uartd.c
@@ -350,7 +350,6 @@ static uartd_respond uartd_process_data(uint_fast8_t port, uint_fast16_t begin, 
 			{ // Found first header byte after leading bytes
 				state->inHeader = true;
 				state->headerPos = 0;
-				state->headerPtr = state->bufferPtr+pos;
 				// Account for delta between first byte of header and interrupt time (which can be assumed to be sending at full speed, as IDLE line would cause interrupt, too)
 				uint64_t bytesFromHeaderToInterrupt = bytesAhead + end - pos - state->numLeading;
 				uint32_t ticksFromHeaderToInterrupt = (bytesFromHeaderToInterrupt * UART_BAUD_PER_BYTE * 1000000) / state->baudrate;

shared/mcu/uartd.h
@@ -50,7 +50,6 @@ typedef struct
 	bool inHeader;
 	bool inData;
 	bool ignoreData;
-	uint8_t *headerPtr;	// Beginning of header in buffer
 	uint_fast8_t headerPos; // Bytes of header that are in headerRaw
 	uint8_t headerRaw[PACKET_HEADER_SIZE+HEADER_CHECKSUM_SIZE]; // Bytes of header to parse
 	struct PacketHeader header; // Parsed header
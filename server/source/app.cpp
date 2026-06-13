AsterTrack/AsterTrack

commit eda0082094abc53ef0af18024c317a69d884069e

Fixed windows compilation errors Also adapted Makefile a bit to (unsuccessful) experiments made on windows

server/source/pipeline/pipeline_tracking.cpp
@@ -649,7 +649,7 @@ void UpdateTrackingPipeline(PipelineState &pipeline, std::vector<CameraPipeline*
 
 		trk0 = pclock::now();
 
-		omp_set_num_threads(std::min<int>(track.trackedTargets.size(), pipeline.params.track.maxParallelism));
+		omp_set_num_threads(std::min<int>(omp_get_max_threads(), pipeline.params.track.maxParallelism));
 	#pragma omp parallel for schedule(dynamic)
 		for (int t = 0; t < track.trackedTargets.size(); t++)
 		{

server/source/target/detection2D.cpp
@@ -86,7 +86,7 @@ TargetMatch2D probeTarget2D(std::stop_token stopToken, const TargetCalibration3D
 	// Evaluate all possible combinations
 	std::vector<std::pair<int,float>> itResults(rotations.size(), { 0, std::numeric_limits<float>::max() });
 	std::vector<TargetMatch2D> itMatches(rotations.size());
-	omp_set_num_threads(params.maxParallelism);
+	omp_set_num_threads(std::min<int>(omp_get_max_threads(), params.maxParallelism));
 #pragma omp parallel for schedule(static, 100)
 	for (int i = 0; i < rotations.size(); i++)
 	{
@@ -259,7 +259,7 @@ static std::vector<Eigen::Isometry3f> bruteForcePoseCandidates(std::stop_token s
 	// Evaluate all possible combinations
 	std::vector<std::pair<int,float>> itResults(permIndices.size(), { 0, std::numeric_limits<float>::max() });
 	std::vector<Eigen::Isometry3f> itPoses(permIndices.size());
-	omp_set_num_threads(params.maxParallelism);
+	omp_set_num_threads(std::min<int>(omp_get_max_threads(), params.maxParallelism));
 #pragma omp parallel for schedule(static, 100)
 	for (int i = 0; i < permIndices.size(); i++)
 	{

server/source/ui/pipeline_calibration_target.cpp
@@ -569,8 +569,8 @@ void InterfaceState::UpdatePipelineTargetCalib()
 			{
 				auto stages_lock = pipeline.targetCalib.assemblyStages.contextualRLock();
 				TargetAssemblyBase &base = stages_lock->back()->base;
-				ImGui::Text("%ld markers, %.4fpx += %.4fpx, %.4fpx max",
-					base.target.markers.size(),
+				ImGui::Text("%d markers, %.4fpx += %.4fpx, %.4fpx max",
+					(int)base.target.markers.size(),
 					base.errors.mean*PixelFactor,
 					base.errors.stdDev*PixelFactor,
 					base.errors.max*PixelFactor);
@@ -1179,9 +1179,9 @@ void InterfaceState::UpdatePipelineTargetCalib()
 			ImGui::TableNextColumn();
 			ImGui::Text("%d", i);
 			ImGui::TableNextColumn();
-			ImGui::Text("%ld", stage.base.merged.size());
+			ImGui::Text("%d", (int)stage.base.merged.size());
 			ImGui::TableNextColumn();
-			ImGui::Text("%ld", stage.base.target.markers.size());
+			ImGui::Text("%d", (int)stage.base.target.markers.size());
 			ImGui::TableNextColumn();
 			ImGui::Text("%d", stage.base.target.totalSamples);
 			ImGui::SetItemTooltip("%d samples, %d inliers, %d outliers",
@@ -1272,7 +1272,7 @@ void InterfaceState::UpdatePipelineTargetCalib()
 				ImGui::TableNextColumn();
 				ImGui::Text("%d", test.id);
 				ImGui::TableNextColumn();
-				ImGui::Text("%ld", test.bestCandidate < 0? 0 : test.candidates[test.bestCandidate].points.size());
+				ImGui::Text("%d", test.bestCandidate < 0? 0 : (int)test.candidates[test.bestCandidate].points.size());
 				ImGui::TableNextColumn();
 				ImGui::Text("%f", test.RMSE);
 				ImGui::TableNextColumn();
@@ -1297,7 +1297,7 @@ void InterfaceState::UpdatePipelineTargetCalib()
 		if (visState.targetCalib.stageSubIndex >= 0)
 		{
 			auto &test = visState.targetCalib.stage->alignResults[visState.targetCalib.stageSubIndex];
-			ImGui::Text("Alignment Candidates (%ld):", test.candidates.size());
+			ImGui::Text("Alignment Candidates (%d):", (int)test.candidates.size());
 			if (ImGui::BeginTable("TargetBase Merge Candidates Table", 3, //4,
 				ImGuiTableFlags_SizingStretchSame | ImGuiTableFlags_ScrollY | ImGuiTableFlags_PadOuterX,
 				ImVec2(0, ImGui::GetFrameHeight()*10)))
@@ -1315,7 +1315,7 @@ void InterfaceState::UpdatePipelineTargetCalib()
 					ImGui::TableNextColumn();
 					ImGui::Text("%d", i);
 					ImGui::TableNextColumn();
-					ImGui::Text("%ld", cand.points.size());
+					ImGui::Text("%d", (int)cand.points.size());
 					ImGui::TableNextColumn();
 					/* ImGui::Text("%f", cand.RMSE);
 					ImGui::TableNextColumn(); */
@@ -1355,9 +1355,9 @@ void InterfaceState::UpdatePipelineTargetCalib()
 				ImGui::TableNextColumn();
 				ImGui::Text("%d", test.id);
 				ImGui::TableNextColumn();
-				ImGui::Text("%ld", test.pointMap.size());
+				ImGui::Text("%d", (int)test.pointMap.size());
 				ImGui::TableNextColumn();
-				ImGui::Text("%ld", test.markers.size() - test.pointMap.size());
+				ImGui::Text("%d", (int)(test.markers.size() - test.pointMap.size()));
 				ImGui::TableNextColumn();
 				ImGui::Text("%.4fpx", test.error.rmse*PixelFactor);
 				ImGui::TableNextColumn();

server/CMakeLists.txt
@@ -113,7 +113,7 @@ target_include_directories(astertrack PRIVATE "${PROJECT_SOURCE_DIR}/dependencie
 target_include_directories(astertrack PRIVATE "${PROJECT_SOURCE_DIR}/dependencies/sources/imgui")
 
 # Specify language features used
-target_compile_features(astertrack PRIVATE cxx_generalized_initializers cxx_static_assert cxx_std_20)
+target_compile_features(astertrack PRIVATE cxx_generalized_initializers cxx_static_assert cxx_std_23)
 
 # Specify definitions
 target_compile_definitions(astertrack PUBLIC GLEW_STATIC

server/Makefile
@@ -3,7 +3,7 @@
 
 # Read implicit compiler defaults if not explicitly specified
 ifneq ($(COMPILER), clang-libc)
-ifneq ($(COMPILER), clang-gnu)
+ifneq ($(COMPILER), clang-std)
 ifneq ($(COMPILER), gcc)
 ifneq '' '$(findstring clang,$(CC))'
 COMPILER=clang-gnu
@@ -35,32 +35,32 @@ endif
 # Set compiler and options used
 ifeq ($(COMPILER), clang-libc)
 # clang compiler
-CXX=clang++
+CXX=clang++ -MMD -MP
 CC=clang
 LD=clang++
 # clang libraries (libc++ + libomp)
 cstd = -stdlib=libc++ -fopenmp=libomp -Wno-gcc-compat
-lstd = -fuse-ld=lld -lc++ -fopenmp=libomp # --rtlib=compiler-rt -lunwind
+lstd = -fuse-ld=lld -lc++ -fopenmp=libomp -Wl,-rpath='$$ORIGIN' # --rtlib=compiler-rt -lunwind
 lto = -flto=thin
 STDLIB = libcpp
 else ifeq ($(COMPILER), clang-std)
 # clang compiler
-CXX=clang++
+CXX=clang++ -MMD -MP
 CC=clang
 LD=clang++
 # gnu library (libstdc++) but using clangs libomp (libgomp config stopped working)
 cstd = -stdlib=libstdc++ -fopenmp=libomp -Wno-gcc-compat
-lstd = -fuse-ld=lld -lstdc++ -fopenmp=libomp --rtlib=libgcc -latomic
+lstd = -fuse-ld=lld -lstdc++ -fopenmp=libomp --rtlib=libgcc -latomic -Wl,-rpath='$$ORIGIN'
 lto = -flto=thin
 STDLIB = libstdcpp
 else ifeq ($(COMPILER), gcc)
 # gcc compiler
-CXX=g++
+CXX=g++ -MMD -MP
 CC=gcc
 LD=g++
 # gnu libraries (libstdc++ + libgomp)
 cstd = -fopenmp -Wno-virtual-move-assign -Wno-var-tracking-assignments
-lstd = -fopenmp -latomic
+lstd = -fopenmp -latomic -Wl,-rpath='$$ORIGIN'
 lto = -flto=auto
 ifeq "$(shell expr `gcc -dumpversion | cut -f1 -d.` \>= 14)" "1"
 	lstd += -lstdc++exp
@@ -187,16 +187,16 @@ sh = ../shared
 dep = dependencies/sources
 b = build
 o = $(b)/$(ENV_PREFIX)$(COMPILER)-$(mode)
-cflags = $(cenv) -Idependencies/include -Idependencies/sources \
-	-DGLEW_STATIC $(cmode) $(march) -ffunction-sections -fpic -mshstk $(idbus) \
-	-ffp-contract=fast -fno-math-errno -fno-signed-zeros -fno-trapping-math
+cflags = -Idependencies/include -Idependencies/sources \
+	$(cenv) $(cmode) $(march) -ffunction-sections -fpic -mshstk $(idbus) \
+	-ffp-contract=fast -fno-math-errno -fno-signed-zeros -fno-trapping-math \
+	-DGLEW_STATIC
 cxxflags = $(cflags) $(cstd) -I$(src) -I$(sh) -I$(dep)/imgui \
-	-DEIGEN_MPL2_ONLY -D_FILE_OFFSET_BITS=64 \
 	-Wno-deprecated-enum-enum-conversion -Wno-unknown-attributes \
-	-DEIGEN_NO_AUTOMATIC_RESIZING -DEIGEN_INITIALIZE_MATRICES_BY_NAN \
+	-DEIGEN_MPL2_ONLY -DEIGEN_NO_AUTOMATIC_RESIZING -DEIGEN_INITIALIZE_MATRICES_BY_NAN \
 	-DBLOB_EMULATION \
 	-std=c++23
-lflags = $(lenv) -Wl,-rpath='$$ORIGIN' $(lstd) $(lmode) $(march)
+lflags = $(lenv) $(lstd) $(lmode) $(march)
 
 
 # Define all source files to be compiled
@@ -284,12 +284,12 @@ SOURCES_INTERFACE = $(addprefix $(src)/,$(INTERFACE_CPP)) $(addprefix $(dep)/,$(
 
 # Find libraries
 libdir = dependencies/lib/linux
-usblibs = $(libdir)/libusb/libusb-1.0.so.0
+usblib = $(libdir)/libusb/libusb-1.0.so.0
 vrpnlibs = $(libdir)/vrpn/libvrpn.a $(libdir)/vrpn/libquat.a
-glfwlibs = $(libdir)/libglfw3.a
+glfwlib = $(libdir)/libglfw3.a
 trackerlibs = -ludev
-server_libs = $(usblibs) $(vrpnlibs) $(trackerlibs) -lrt -lGL -lturbojpeg
-interface_libs = $(glfwlibs) -ldbus-1
+server_libs = $(usblib) $(vrpnlibs) $(trackerlibs) -lrt -lGL -lturbojpeg
+interface_libs = $(glfwlib) -ldbus-1
 
 # Run prechecks before starting compilation
 checkLibs := $(shell $(ldconfig) | grep libturbojpeg > /dev/null 2>&1; echo $$?)
@@ -312,13 +312,13 @@ checkLibs := $(shell test -d "dependencies/include/vrpn"; echo $$?)
 ifneq ($(checkLibs),0)
   $(error vrpn include files are missing, make sure to build dependencies)
 endif
-checkLibs := $(shell test -f "$(libdir)/libglfw3.a"; echo $$?)
+checkLibs := $(shell test -f "$(glfwlib)"; echo $$?)
 ifneq ($(checkLibs),0)
-  $(error GLFW library is missing ($(libdir)/libglfw3.a), make sure to build dependencies)
+  $(error GLFW library is missing ($(glfwlib)), make sure to build dependencies)
 endif
-checkLibs := $(shell test -d "$(libdir)/libusb"; echo $$?)
+checkLibs := $(shell test -f "$(usblib)"; echo $$?)
 ifneq ($(checkLibs),0)
-  $(error libusb library is missing ($(libdir)/libusb), make sure to build dependencies)
+  $(error libusb library is missing ($(usblib)), make sure to build dependencies)
 endif
 checkLibs := $(shell test -d "$(libdir)/vrpn"; echo $$?)
 ifneq ($(checkLibs),0)
@@ -332,10 +332,9 @@ all: $(o)/astertrack-interface.so $(o)/astertrack-server $(o)/astertrack-static 
 
 .PHONY: copybinaries
 copybinaries: $(o)/astertrack-interface.so $(o)/astertrack-server $(o)/astertrack-static
-	@cp -u $(usblibs) "$(b)/"
-	@-rm -f $(b)/astertrack-*
-	@cd 
-	@ln -sr $(o)/astertrack-* $(b)/
+	@cp -u $(usblib) "$(b)/"
+	@-rm -f $(b)/astertrack*
+	@ln -sr $(o)/astertrack* $(b)/
 
 
 # May prefix this before each target to measure compile times
@@ -343,13 +342,13 @@ copybinaries: $(o)/astertrack-interface.so $(o)/astertrack-server $(o)/astertrac
 
 # Generic compile targets
 $(o)/%.obj: $(src)/%.cpp
-	$(CXX) -MMD -MP $(cxxflags) -c $< -o $@
+	$(CXX) $(cxxflags) -c $< -o $@
 $(o)/shared/%.obj: $(sh)/%.cpp
-	$(CXX) -MMD -MP $(cxxflags) -c $< -o $@
+	$(CXX) $(cxxflags) -c $< -o $@
 $(o)/shared/%.obj: $(sh)/%.c
 	$(CC) $(cflags) -c $< -o $@
 $(o)/dependencies/%.obj: $(dep)/%.cpp
-	$(CXX) -MMD -MP $(cxxflags) -c $< -o $@
+	$(CXX) $(cxxflags) -c $< -o $@
 $(o)/dependencies/%.obj: $(dep)/%.c
 	$(CC) $(cflags) -c $< -o $@
 
@@ -367,9 +366,9 @@ $(shell mkdir -p $(dir $(OBJECTS_INTERFACE)) >/dev/null)
 # Combined server and interface just to have compile-time validation of interface
 OBJECTS_STATIC = $(subst $(o)/app.obj,$(o)/app-static.obj,$(OBJECTS_SERVER)) $(OBJECTS_INTERFACE)
 $(o)/app-static.obj: $(src)/app.cpp
-	$(CXX) -MMD -MP $(cxxflags) -DINTERFACE_LINKED -c $< -o $@
+	$(CXX) $(cxxflags) -DINTERFACE_LINKED -c $< -o $@
 $(o)/astertrack-static: $(OBJECTS_STATIC)
-	$(LD) $(lenv) -rdynamic -o $@ $(OBJECTS_STATIC) $(server_libs) $(interface_libs) $(lflags)
+	$(LD) $(lenv) -o $@ $(OBJECTS_STATIC) $(server_libs) $(interface_libs) $(lflags)
 	objcopy --only-keep-debug $@ $@.debug
 	strip -S $@
 	objcopy --add-gnu-debuglink=$@.debug $@

server/source/config.cpp
@@ -1046,7 +1046,7 @@ std::optional<ErrorMessage> parseRecording(const std::string &path, std::vector<
 		cameraIndices.resize(parsedCameras.size(), -1);
 		if (separate || cameras.size() == priorCameras)
 		{ // Append any new cameras
-			std::iota(cameraIndices.begin()+priorCameras, cameraIndices.end(), cameras.size());
+			std::iota(cameraIndices.begin()+priorCameras, cameraIndices.end(), (int)cameras.size());
 			std::move(parsedCameras.begin()+priorCameras, parsedCameras.end(), std::back_inserter(cameras));
 		}
 		else
@@ -1867,7 +1867,7 @@ std::optional<ErrorMessage> parseTargetObjFile(const std::string &path, std::map
 	std::ifstream fs(path);
 	if (!fs.is_open()) return asprintf_s("Failed to open '%s' for reading!", path.c_str());
 
-	std::string label = std::filesystem::path(path).stem();
+	std::string label = std::filesystem::path(path).stem().string();
 	std::map<std::string, TargetCalibration3D> groups = { { label, TargetCalibration3D() } };
 	TargetCalibration3D *curGroup = &groups[label];
 

server/source/device/camera_firmware.cpp
@@ -1032,7 +1032,7 @@ FirmwareUpdateRef CamerasFlashFirmwareFile(std::vector<std::shared_ptr<TrackingC
 
 					bool isDir = mz_zip_reader_is_file_a_directory(&zip_archive, i);
 					LOG(LFirmwareUpdate, LInfo, "FW File: \"%s\", Comment: \"%s\", Uncompressed size: %u, Compressed size: %u, Is Dir: %u",
-						file_stat.m_filename, file_stat.m_comment, (uint)file_stat.m_uncomp_size, (uint)file_stat.m_comp_size, isDir);
+						file_stat.m_filename, file_stat.m_comment, (unsigned int)file_stat.m_uncomp_size, (unsigned int)file_stat.m_comp_size, isDir);
 					if (isDir) continue;
 
 					if (!file_stat.m_is_supported)

server/source/server.hpp
@@ -116,16 +116,16 @@ struct ServerState
 	// Tracker Replacement
 	bool keepUnmatchedObservations = true;
 	// Loaded records for replay
+	struct Segment { FrameNum frameStart, frameCount, frameOffset; };
+	struct Recording { int number; std::string label; FrameNum frameStart, frameCount; std::vector<int> cameras; };
 	struct {
 		// This may contain info about multiple appended recordings
 		// And each recording may be split into multiple segments (capture+tracking)
 		// The following lists have an entry for each segment
 		std::vector<std::string> captures;
 		std::vector<std::string> tracking;
-		struct Segment { FrameNum frameStart, frameCount, frameOffset; };
 		std::vector<Segment> segments;
 		// This list has an entry for each appended recording
-		struct Recording { int number; std::string label; FrameNum frameStart, frameCount; std::vector<int> cameras; };
 		std::vector<Recording> recordings;
 		FrameNum frames = 0;
 		TimePoint_t replayTime;

commit 06bdb760d91345a39110950e61816ea9e36d10e6

Fixed excessive OpenMP resource usage Performance regression introduced in eda0082 - Fixed calling omp_set_num_threads with 0 - but instead force used all threads (8 by default) - due to wait policy this kept them spinlocking constantly - so CPU usage appeared as if all threads were running full throttle
Fix:
Stop using omp_set_max_threads, that's global, use num_threads instead
- num_threads should also ensure a min of 1,  fixing the original issue
Attempt to use dynamic so parallel MAY use fewer threads
- this doesn't actually seem to work with libomp
Set wait policy to passive to reduce idle thread overhead
- this prevents those extra threads from spinlocking
- they would previously not reliably sleep since they were used intermittently
- theoretically this may increase processing latency but that's preferrable

server/source/app.cpp
@@ -46,6 +46,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "ctpl/ctpl.hpp"
 extern ctpl::thread_pool threadPool;
 
+#include <omp.h>
+
 #include "crash_handler.inl"
 
 #include "comm/packet.h"
@@ -152,6 +154,13 @@ static bool rotate_log_files(std::string logPath);
 
 int main (void)
 {
+	// Configure OpenMP
+	static char OMP_ENV[] = "OMP_WAIT_POLICY=PASSIVE";
+    putenv(OMP_ENV); // Terrible, horrible hack, but OpenMP leaves me no choice...
+	omp_set_num_threads(omp_get_max_threads());
+	omp_set_dynamic(true);
+	// NOTE: Keep putenv as early as possible! Not threadsafe and may conflict with future desktop integrations
+
 	// Set interface pointers to NOP functions
 	UnlinkInterface();
 

server/source/calib/opt/ReprojectionError.hpp
@@ -460,7 +460,7 @@ struct ReprojectionError
 							tgtFrameIndices.push_back(std::make_pair(f, sampleIndex));
 							sampleIndex += target.frames[f].samples.size();
 						}
-					#pragma omp parallel for schedule(dynamic)
+					#pragma omp parallel for schedule(static)
 						for (int f = 0; f < tgtFrameIndices.size(); f++)
 						{
 							int frameIndex = tgtFrameIndices[f].first;

server/source/calib_point/reconstruction_recovery.cpp
@@ -294,8 +294,7 @@ float determineRank4Basis(
 #ifdef PARALLEL
 	std::atomic<int> totalPartN = 0, totalColsN = 0;
 
-	omp_set_num_threads(std::min<int>(omp_get_max_threads(), params.basis.maxParallelism));
-	#pragma omp parallel
+	#pragma omp parallel num_threads(params.basis.maxParallelism)
 #endif
 	{
 	std::vector<int> selectablePoints;
@@ -306,7 +305,7 @@ float determineRank4Basis(
 
 	auto p0 = sclock::now();
 
-	#pragma omp for nowait schedule(static, 100) reduction(+:colsN) reduction(+:tested4Tuples)
+	#pragma omp for nowait schedule(static, 10) reduction(+:colsN) reduction(+:tested4Tuples)
 	for (int j = 0; j < max4TupleTests; j++)
 #else
 	while (colsN < maxNColumns && partN.size() < max4Tuples && tested4Tuples++ < max4TupleTests && !stopToken.stop_requested())
@@ -438,9 +437,6 @@ float determineRank4Basis(
 	}
 #endif
 	}
-#ifdef PARALLEL
-	omp_set_num_threads(omp_get_max_threads());
-#endif
 
 	if (stopToken.stop_requested())
 		return NAN;

server/source/pipeline/pipeline_calibration_target.cpp
@@ -900,7 +900,6 @@ static std::shared_ptr<TargetView> selectCentralTargetView(const std::vector<std
 	LOGC(LInfo, "-- Finding best view to use as base among %ld views!", targetViews.size());
 
 	std::vector<float> baseScores(targetViews.size());
-#pragma omp parallel for schedule(dynamic)
 	for (int i = 0; i < targetViews.size(); i++)
 	{
 		auto &viewPtr = targetViews[i];

server/source/pipeline/pipeline_tracking.cpp
@@ -649,8 +649,7 @@ void UpdateTrackingPipeline(PipelineState &pipeline, std::vector<CameraPipeline*
 
 		trk0 = pclock::now();
 
-		omp_set_num_threads(std::min<int>(omp_get_max_threads(), pipeline.params.track.maxParallelism));
-	#pragma omp parallel for schedule(dynamic)
+	#pragma omp parallel for schedule(dynamic) num_threads(std::min<int>(track.trackedTargets.size(), pipeline.params.track.maxParallelism))
 		for (int t = 0; t < track.trackedTargets.size(); t++)
 		{
 			auto &tracker = *std::next(track.trackedTargets.begin(), t);
@@ -666,7 +665,6 @@ void UpdateTrackingPipeline(PipelineState &pipeline, std::vector<CameraPipeline*
 				frame->time, frame->num, camCount, pipeline.params.track);
 			tracker.procTimeMS = dtMS(start, sclock::now());
 		}
-		omp_set_num_threads(omp_get_max_threads());
 
 		// Update mistrust rating of each tracker based on matched points
 		// Use tracked targets directly as remainingPoints2D have not yet been updated

server/source/server.cpp
@@ -48,7 +48,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <numeric> // iota
 
 #include "ctpl/ctpl.hpp"
-#include <omp.h>
 
 #include "hidapi/hidapi.h"
 
@@ -116,8 +115,6 @@ bool ServerInit(ServerState &state)
 		}
 		DebugSpecificCameraParameters(state.cameraCalibrations, modes);
 	}
-
-	omp_set_num_threads(omp_get_max_threads());
 
 	if (socket_initialise())
 	{

server/source/target/detection2D.cpp
@@ -86,8 +86,7 @@ TargetMatch2D probeTarget2D(std::stop_token stopToken, const TargetCalibration3D
 	// Evaluate all possible combinations
 	std::vector<std::pair<int,float>> itResults(rotations.size(), { 0, std::numeric_limits<float>::max() });
 	std::vector<TargetMatch2D> itMatches(rotations.size());
-	omp_set_num_threads(std::min<int>(omp_get_max_threads(), params.maxParallelism));
-#pragma omp parallel for schedule(static, 100)
+#pragma omp parallel for schedule(static) num_threads(params.maxParallelism)
 	for (int i = 0; i < rotations.size(); i++)
 	{
 		const auto &quat = rotations[i];
@@ -148,7 +147,6 @@ TargetMatch2D probeTarget2D(std::stop_token stopToken, const TargetCalibration3D
 			itMatches[i] = targetMatch2D;
 		}
 	}
-	omp_set_num_threads(omp_get_max_threads());
 	if (stopToken.stop_requested())
 		return {};
 
@@ -259,8 +257,7 @@ static std::vector<Eigen::Isometry3f> bruteForcePoseCandidates(std::stop_token s
 	// Evaluate all possible combinations
 	std::vector<std::pair<int,float>> itResults(permIndices.size(), { 0, std::numeric_limits<float>::max() });
 	std::vector<Eigen::Isometry3f> itPoses(permIndices.size());
-	omp_set_num_threads(std::min<int>(omp_get_max_threads(), params.maxParallelism));
-#pragma omp parallel for schedule(static, 100)
+#pragma omp parallel for schedule(static) num_threads(params.maxParallelism)
 	for (int i = 0; i < permIndices.size(); i++)
 	{
 		const auto &perm = permIndices[i];
@@ -288,7 +285,6 @@ static std::vector<Eigen::Isometry3f> bruteForcePoseCandidates(std::stop_token s
 			}
 		}
 	}
-	omp_set_num_threads(omp_get_max_threads());
 	if (stopToken.stop_requested())
 		return {};
 
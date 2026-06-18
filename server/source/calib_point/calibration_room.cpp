AsterTrack/AsterTrack

commit 568ae1a8d72cd7302dfe7754acb3832457939dba

Switch least-squares problems to use SVD instead of EVD EVD, while faster, does not solve in the least-squares sense So for critical calibration problems the increased accuracy of SVD is much preferred over a bit more speed

server/source/calib_point/calibration_room.cpp
@@ -277,13 +277,9 @@ HANDLE_ERROR estimateFloorTransform(const std::vector<CameraCalib_t<Scalar>> &ca
 		if (floorPoints[i].confidence > 1)
 			N.col(index++) = floorPoints[i].pos-origin;
 	}
-	// TODO: Choose either SVD or EVD
-	/* Eigen::JacobiSVD<Matrix3<Scalar>, Eigen::ComputeFullU> svd_N(N);
-	Vector3<Scalar> axis = svd_N.matrixU().block<3,1>(0, 2);
-	Vector3<Scalar> rankValues = svd_N.singularValues().tail<3>().reverse(); */
-	Eigen::SelfAdjointEigenSolver<Matrix3<Scalar>> evd_N(N*N.transpose(), Eigen::ComputeEigenvectors);
-	Vector3<Scalar> axis = evd_N.eigenvectors().template block<3,1>(0, 0);
-	Vector3<Scalar> rankValues = evd_N.eigenvalues().template head<3>();
+	Eigen::BDCSVD<Matrix3<Scalar>, Eigen::ComputeFullU> svd_N(N);
+	Vector3<Scalar> axis = svd_N.matrixU().template block<3,1>(0, 2);
+	Vector3<Scalar> rankValues = svd_N.singularValues().template tail<3>().reverse();
 	LOG(LPointCalib, LDebug, "    PCA for rotation has ranks (%f, %f, %f)\n", rankValues(2), rankValues(1), rankValues(0));
 
 	if (rankValues(0)*1000 > rankValues(1))
@@ -340,13 +336,9 @@ void getCalibNormalisation(const std::vector<CameraCalib_t<Scalar>> &calibs, Mat
 		if (!calib.invalid())
 			N.col(c++) = calib.transform.translation()-origin;
 
-	// TODO: Choose either SVD or EVD
-	Eigen::JacobiSVD<Eigen::Matrix3X<Scalar>, Eigen::ComputeFullU> svd_N(N);
+	Eigen::BDCSVD<Eigen::Matrix3X<Scalar>, Eigen::ComputeFullU> svd_N(N);
 	Vector3<Scalar> axis = svd_N.matrixU().template block<3,1>(0, 2);
 	//Vector3<Scalar> rankValues = svd_N.singularValues().template tail<3>().reverse();
-	/* Eigen::SelfAdjointEigenSolver<Eigen::Matrix3X<Scalar>> evd_N(N*N.transpose(), Eigen::ComputeEigenvectors);
-	Vector3<Scalar> axis = evd_N.eigenvectors().template block<3,1>(0, 0);
-	Vector3<Scalar> rankValues = evd_N.eigenvalues().template head<3>(); */
 
 	// Flip room up axis based on camera positions
 	Scalar flipAxis = 0;

server/source/calib_point/reconstruction.cpp
@@ -706,13 +706,9 @@ static Eigen::MatrixXd determineRank4Basis(const Eigen::Ref<const Eigen::MatrixX
 	}
 
 	// Get orthogonal complement of dimension 4, which is the target vector space of M (up to noise ofc)
-	// TODO: Choose either SVD or EVD
-	/*Eigen::BDCSVD<Eigen::MatrixXd, Eigen::ComputeFullU> svd_N(N);
-	basis = svd_N.matrixU().block(0, recViewCount*3-4, recViewCount*3, 4);
-	Eigen::Matrix<double,5,1> rankValues = svd_N.singularValues().tail<5>().reverse();*/
-	Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> evd_N(N*N.transpose(), Eigen::ComputeEigenvectors);
-	Eigen::MatrixXd basis = evd_N.eigenvectors().block(0, 0, viewCount*3, 4);
-	Eigen::Matrix<double,5,1> rankValues = evd_N.eigenvalues().head<5>();
+	Eigen::BDCSVD<Eigen::MatrixXd, Eigen::ComputeFullU> svd_N(N);
+	Eigen::MatrixXd basis = svd_N.matrixU().block(0, viewCount*3-4, viewCount*3, 4);
+	Eigen::Matrix<double,5,1> rankValues = svd_N.singularValues().tail<5>().reverse();
 
 	if (rankValues(3)*params.basis.minRankFactor > rankValues(4))
 	{ // Expected full rank due to noise, less than this means not enough 4-tuples were added

server/source/util/eigenalg.hpp
@@ -180,30 +180,19 @@ static Eigen::Transform<Scalar,3,Type> kabsch(Eigen::Ref<Eigen::Matrix<Scalar,Ei
 template<typename Scalar>
 Scalar solveFundamentalMatrix(const Eigen::Matrix<Scalar,Eigen::Dynamic,Eigen::Dynamic> &dataMatrix, Eigen::Matrix<Scalar,3,3> &fundamentalMatrix)
 {
-	// Solve dataMatrix * f = 0 in least square sense
-	// TODO: Choose either SVD or EVD based on speed and accuracy
-
-	typedef Eigen::Matrix<Scalar,Eigen::Dynamic,Eigen::Dynamic> MatrixX;
-	typedef Eigen::Matrix<Scalar,3,3> Matrix3;
-
-	// Option: SVD
-	/*Eigen::BDCSVD<MatrixX, Eigen::ComputeThinV> svd(dataMatrix);
+	// While SelfAdjointEigenSolver would be faster, SVD solves dataMatrix * f = 0 more accurately in the least squares sense
+	Eigen::BDCSVD<MatrixX<Scalar>, Eigen::ComputeThinV> svd(dataMatrix);
 	Eigen::Matrix<Scalar,9,1> fv = svd.matrixV().col(8);
-	Eigen::Matrix<Scalar,9,1> rankValues = svd.singularValues();*/
-
-	// Option: EVD
-	Eigen::SelfAdjointEigenSolver<Eigen::Matrix<Scalar,9,9>> evd(dataMatrix.transpose()*dataMatrix, Eigen::ComputeEigenvectors);
-	Eigen::Matrix<Scalar,9,1> fv = evd.eigenvectors().col(0);
-	Eigen::Matrix<Scalar,9,1> rankValues = evd.eigenvalues().reverse();
+	Eigen::Matrix<Scalar,9,1> rankValues = svd.singularValues();
 
 	//LOGC(LTrace, "Fundamental Matrix Rank Values: %f, %f, %f, %f, %f, %f, %f, %f, %f\n", rankValues(0), rankValues(1), rankValues(2), rankValues(3), rankValues(4), rankValues(5), rankValues(6), rankValues(7), rankValues(8));
 
 	// Assemble coefficients to fundamental matrix
-	Matrix3 F;
+	Matrix3<Scalar> F;
 	F << fv(0), fv(1), fv(2), fv(3), fv(4), fv(5), fv(6), fv(7), fv(8);
 
 	// Decompose Fd by using SVD to expose singular values
-	Eigen::JacobiSVD<Matrix3, Eigen::ComputeFullU | Eigen::ComputeFullV> svd_rank(F);
+	Eigen::JacobiSVD<Matrix3<Scalar>, Eigen::ComputeFullU | Eigen::ComputeFullV> svd_rank(F);
 	//LOGC(LTrace, "Fundamental matrix singular values before rank-2 truncation: %f, %f, %f\n", svd_rank.singularValues().x(), svd_rank.singularValues().y(), svd_rank.singularValues().z());
 
 	// Reduce rank to 2 by removing the least significant (third) singular value

commit 4352e5a1cd75a951e3ed96b9d0c0780b6534629d

Fixed room calibration errors Fixed regression introduced in 568ae1a: - Used Matrix3 when one axis is dynamic with SVD (compared to EVD used previously) Relaxed rank factor limits due to different behaviour of SVD - For the same set of points on a plane SVD would have a rank factor approximately 10x lower Fixed use of sample point without samples - May occur when at end of replay, when point cannot finish sampling - If it is then calibrated, it would call update without any samples, causing a crash Fixed deadlock by copying room calibration data in thread

server/source/calib_point/calibration_room.cpp
@@ -230,6 +230,11 @@ OptErrorRes determinePointOutliers(ObsData &data, const std::vector<CameraCalib>
 template<typename Scalar>
 void StaticPointSamples<Scalar>::update(const std::vector<CameraCalib> &calibs)
 {
+	if (sampleCount == 0)
+	{
+		confidence = 0;
+		return;
+	}
 	std::vector<std::vector<Vector2<Scalar>>> samplePoints(calibs.size());
 	std::vector<const std::vector<Vector2<Scalar>>*> samplePointGroups(calibs.size());
 	TriangulatedPoint_t<Scalar> sampleTri(pos, 0, 0, calibs.size());
@@ -278,16 +283,16 @@ HANDLE_ERROR estimateFloorTransform(const std::vector<CameraCalib_t<Scalar>> &ca
 		if (floorPoints[i].confidence > 1)
 			N.col(index++) = floorPoints[i].pos-origin;
 	}
-	Eigen::BDCSVD<Matrix3<Scalar>, Eigen::ComputeFullU> svd_N(N);
+	Eigen::BDCSVD<MatrixX<Scalar>, Eigen::ComputeFullU> svd_N(N);
 	Vector3<Scalar> axis = svd_N.matrixU().template block<3,1>(0, 2);
 	Vector3<Scalar> rankValues = svd_N.singularValues().template tail<3>().reverse();
 	LOG(LPointCalib, LDebug, "    PCA for rotation has ranks (%f, %f, %f)\n", rankValues(2), rankValues(1), rankValues(0));
 
-	if (rankValues(0)*1000 > rankValues(1))
-		return asprintf_s("The floor points don't appear to be on a plane or are too noisy!");
+	if (rankValues(1)/rankValues(0) < 100)
+		return asprintf_s("The floor points don't appear to be on a plane or are too noisy! Rank factor %.3f < 100", rankValues(1)/rankValues(0));
 
-	if (rankValues(1)*20 < rankValues(2))
-		return asprintf_s("The floor points appear to be close to a straight line - make sure they span a plane!");
+	if (rankValues(2)/rankValues(1) > 20)
+		return asprintf_s("The floor points appear to be close to a straight line - make sure they span a plane! Rank factor %.3f > 20", rankValues(2)/rankValues(1));
 
 	// TODO: Account for marker size by shifting floor plane up by their radius
 	// Supporting varying sizes sounds like a pain though

server/source/pipeline/pipeline_calibration_point.cpp
@@ -530,24 +530,22 @@ static void ThreadCalibrationRoom(PipelineState *pipeline, std::vector<CameraPip
 
 	LOGC(LInfo, "=======================\n");
 
-	auto roomCalib = pipeline->pointCalib.room.contextualLock();
-	std::vector<CameraCalib> calibs(pipeline->cameras.size());
+	// Copy data
+	auto roomCalib = *pipeline->pointCalib.room.contextualRLock();
+	std::vector<CameraCalib> calibs(cameras.size());
 	for (int c = 0; c < cameras.size(); c++)
-	{
-		int index = cameras[c]->index;
-		calibs[index] = cameras[c]->calib;
-	}
+		calibs[c] = cameras[c]->calib;
 
 	LOG(LPointCalib, LDebug, "Attempting to calibrate the floor!\n");
 
 	// Re-evaluate positions in case calibration changed since observation
-	for (auto &point : roomCalib->floorPoints)
+	for (auto &point : roomCalib.floorPoints)
 		point.update(calibs);
 
 	// Estimate a transform to align floor plane to points with correct scale
 	Eigen::Matrix3d roomOrientation;
 	Eigen::Affine3d roomTransform;
-	auto error = estimateFloorTransform<double>(calibs, roomCalib->floorPoints, roomCalib->distance12, roomOrientation, roomTransform);
+	auto error = estimateFloorTransform<double>(calibs, roomCalib.floorPoints, roomCalib.distance12, roomOrientation, roomTransform);
 	if (error)
 	{
 		LOG(LPointCalib, LError, "Failed to calibrate the floor: %s", error->c_str());
@@ -555,7 +553,7 @@ static void ThreadCalibrationRoom(PipelineState *pipeline, std::vector<CameraPip
 	}
 	else
 	{
-		LOG(LPointCalib, LInfo, "Successfully calibrated floor with %d points!\n", (int)roomCalib->floorPoints.size());
+		LOG(LPointCalib, LInfo, "Successfully calibrated floor with %d points!\n", (int)roomCalib.floorPoints.size());
 		LOG(LPointCalib, LInfo, "Scaled calibration by %f during floor calibration!",
 			roomTransform.linear().colwise().norm().mean());
 
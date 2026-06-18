81984859f18b6c416543913efc96fd5fd49f68a5
Seneral <seneral@seneral.dev>
2026-06-18 23:45:14 +0200

    Rename integration variables Separated from last commit for clarity

--- server/source/io/integrations.cpp
+++ server/source/io/integrations.cpp
@@ -25,134 +25,134 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "util/eigenutil.hpp"
 
 
-void IntegrationsInit(IntegrationsState &state , const GeneralConfig &config)
+void IntegrationsInit(IntegrationsState &io, const GeneralConfig &config)
 {
-	auto io_lock = std::unique_lock(state.mutex);
-	state.vrpn.enabled = config.integrations.vrpn_auto_enable;
-	state.vmc.enabled = config.integrations.vmc_auto_enable;
-	state.lastUpdatedCameras = sclock::now() - std::chrono::seconds(2);
+	auto io_lock = std::unique_lock(io.mutex);
+	io.vrpn.enabled = config.integrations.vrpn_auto_enable;
+	io.vmc.enabled = config.integrations.vmc_auto_enable;
+	io.lastUpdatedCameras = sclock::now() - std::chrono::seconds(2);
 
-	IntegrationsUpdateNonessentialConfig(state, config);
-	IntegrationsReconfigureVRPN(state, config);
-	IntegrationsReconfigureVMC(state, config);
+	IntegrationsUpdateNonessentialConfig(io, config);
+	IntegrationsReconfigureVRPN(io, config);
+	IntegrationsReconfigureVMC(io, config);
 }
 
-void IntegrationsCleanup(IntegrationsState &state, const GeneralConfig &config)
+void IntegrationsCleanup(IntegrationsState &io, const GeneralConfig &config)
 {
-	auto io_lock = std::unique_lock(state.mutex);
-	state.vrpn.enabled = false;
-	state.vmc.enabled = false;
+	auto io_lock = std::unique_lock(io.mutex);
+	io.vrpn.enabled = false;
+	io.vmc.enabled = false;
 
-	IntegrationsReconfigureVRPN(state, config);
-	IntegrationsReconfigureVMC(state, config);
+	IntegrationsReconfigureVRPN(io, config);
+	IntegrationsReconfigureVMC(io, config);
 }
 
-void IntegrationsUpdateNonessentialConfig(IntegrationsState &state, const GeneralConfig &config)
+void IntegrationsUpdateNonessentialConfig(IntegrationsState &io, const GeneralConfig &config)
 {
 	auto &cfg = config.integrations;
-	state.vrpn.lowLatencySend = cfg.vrpn_low_latency;
+	io.vrpn.lowLatencySend = cfg.vrpn_low_latency;
 }
 
-void IntegrationsReconfigureVRPN(IntegrationsState &state, const GeneralConfig &config)
+void IntegrationsReconfigureVRPN(IntegrationsState &io, const GeneralConfig &config)
 {
-	state.vrpn.trackers.clear();
-	state.vrpn.server = nullptr;
-	if (!state.vrpn.enabled) return;
+	io.vrpn.trackers.clear();
+	io.vrpn.server = nullptr;
+	if (!io.vrpn.enabled) return;
 	// Setup VRPN Connection
 	auto &cfg = config.integrations;
 	std::string host = cfg.vrpn_host.empty()? "localhost" : cfg.vrpn_host;
 	int port = cfg.vrpn_port? cfg.vrpn_port : vrpn_DEFAULT_LISTEN_PORT_NO;
-	state.vrpn.host = host + ":" + std::to_string(port);
+	io.vrpn.host = host + ":" + std::to_string(port);
 	std::string cname = cfg.vrpn_host + ":" + std::to_string(port);
-	state.vrpn.server = opaque_ptr<vrpn_Connection>(vrpn_create_server_connection(cname.c_str()));
-	state.vrpn.server->setAutoDeleteStatus(true);
+	io.vrpn.server = opaque_ptr<vrpn_Connection>(vrpn_create_server_connection(cname.c_str()));
+	io.vrpn.server->setAutoDeleteStatus(true);
 }
 
-void IntegrationsReconfigureVMC(IntegrationsState &state, const GeneralConfig &config)
+void IntegrationsReconfigureVMC(IntegrationsState &io, const GeneralConfig &config)
 {
-	state.vmc.output = nullptr;
-	if (!state.vmc.enabled) return;
+	io.vmc.output = nullptr;
+	if (!io.vmc.enabled) return;
 	// Setup VRPN Connection
 	auto &cfg = config.integrations;
 	std::string host = cfg.vmc_host.empty()? "localhost" : cfg.vmc_host;
 	int port = cfg.vmc_port? cfg.vmc_port : vmc_DEFAULT_PERFORMER_PORT_NO;
-	state.vmc.host = host + ":" + std::to_string(port);
-	state.vmc.output = vmc_init_output(host, std::to_string(port));
-	vmc_try_open(state.vmc.output, state.vmc.host);
+	io.vmc.host = host + ":" + std::to_string(port);
+	io.vmc.output = vmc_init_output(host, std::to_string(port));
+	vmc_try_open(io.vmc.output, io.vmc.host);
 }
 
-void IntegrationsUpdate(IntegrationsState &state, ServerState &server)
+void IntegrationsUpdate(IntegrationsState &io, ServerState &state)
 { // Gets called regularly while in a mode, even when not streaming
-	auto io_lock = std::unique_lock(state.mutex);
+	auto io_lock = std::unique_lock(io.mutex);
 
 	// Timestamp for camera position
 	TimePoint_t timestamp = sclock::now();
-	OptFrameNum frame = server.pipeline.frameNum.load();
+	OptFrameNum frame = state.pipeline.frameNum.load();
 	if (frame > 0)
 	{ // Replay may not be realtime, so take last frame time
-		auto view = server.pipeline.record.frames.getView();
+		auto view = state.pipeline.record.frames.getView();
 		if (frame < view.endIndex())
 			timestamp = view[frame]->time;
 	}
 
-	bool updateCameras = dtS(state.lastUpdatedCameras, sclock::now()) > 1;
+	bool updateCameras = dtS(io.lastUpdatedCameras, sclock::now()) > 1;
 	if (updateCameras)
-		state.lastUpdatedCameras = sclock::now();
+		io.lastUpdatedCameras = sclock::now();
 
-	if (state.vrpn.enabled)
+	if (io.vrpn.enabled)
 	{
-		for (auto &tracker : server.trackerConfigs)
+		for (auto &tracker : state.trackerConfigs)
 		{
-			auto io_tracker = state.vrpn.trackers.find(tracker.id);
+			auto io_tracker = io.vrpn.trackers.find(tracker.id);
 			if (!tracker.exposed)
 			{
-				if (io_tracker != state.vrpn.trackers.end())
+				if (io_tracker != io.vrpn.trackers.end())
 				{ // Disconnect after expose was disabled
 					LOG(LIO, LInfo, "VRPN Tracker '%s' is no longer exposed!", tracker.label.c_str());
 					if (io_tracker->second->markedConnected)
 						tracker.connected--;
-					state.vrpn.trackers.erase(io_tracker);
-					if (server.trackerOutput.contains(tracker.id))
-						server.trackerOutput[tracker.id].vrpn = nullptr;
-					ServerUpdateTrackerConditions(server, tracker);
+					io.vrpn.trackers.erase(io_tracker);
+					if (state.trackerOutput.contains(tracker.id))
+						state.trackerOutput[tracker.id].vrpn = nullptr;
+					ServerUpdateTrackerConditions(state, tracker);
 				}
 				continue;
 			}
-			if (io_tracker == state.vrpn.trackers.end())
+			if (io_tracker == io.vrpn.trackers.end())
 			{
 				LOG(LIO, LInfo, "Exposing VRPN Tracker '%s'", tracker.label.c_str());
-				auto vrpn_tracker = std::make_shared<vrpn_Tracker_AsterTrack>(tracker.id, tracker.label.c_str(), state.vrpn.server.get());
-				if (server.trackerOutput.contains(tracker.id))
-					server.trackerOutput[tracker.id].vrpn = vrpn_tracker;
-				io_tracker = state.vrpn.trackers.insert({ tracker.id, std::move(vrpn_tracker) }).first;
+				auto vrpn_tracker = std::make_shared<vrpn_Tracker_AsterTrack>(tracker.id, tracker.label.c_str(), io.vrpn.server.get());
+				if (state.trackerOutput.contains(tracker.id))
+					state.trackerOutput[tracker.id].vrpn = vrpn_tracker;
+				io_tracker = io.vrpn.trackers.insert({ tracker.id, std::move(vrpn_tracker) }).first;
 			}
 			if (!io_tracker->second->markedConnected && io_tracker->second->isConnected())
 			{
 				LOG(LIO, LInfo, "VRPN Tracker '%s' has been connected!", tracker.label.c_str());
 				io_tracker->second->markedConnected = true;
 				tracker.connected++;
-				ServerUpdateTrackerConditions(server, tracker);
+				ServerUpdateTrackerConditions(state, tracker);
 			}
 			else if (io_tracker->second->markedConnected && !io_tracker->second->isConnected())
 			{
 				LOG(LIO, LInfo, "VRPN Tracker '%s' has been disconnected!", tracker.label.c_str());
 				io_tracker->second->markedConnected = false;
 				tracker.connected--;
-				ServerUpdateTrackerConditions(server, tracker);
+				ServerUpdateTrackerConditions(state, tracker);
 			}
 		}
 
 		if (updateCameras)
 		{ // Add cameras as trackers to give clients an opportunity to display them as references
 			// TODO: Implement proper custom protocol for meta-information like cameras, single 3D markers, etc.
-			for (const auto &camera : server.pipeline.cameras)
+			for (const auto &camera : state.pipeline.cameras)
 			{
-				auto io_tracker = state.vrpn.trackers.find(camera->id);
-				if (io_tracker == state.vrpn.trackers.end())
+				auto io_tracker = io.vrpn.trackers.find(camera->id);
+				if (io_tracker == io.vrpn.trackers.end())
 				{
 					std::string path = asprintf_s("AsterTrack_Camera_%d", camera->index);
-					auto vrpn_tracker = std::make_shared<vrpn_Tracker_AsterTrack>(camera->id, path.c_str(), state.vrpn.server.get());
-					io_tracker = state.vrpn.trackers.insert({ camera->id, std::move(vrpn_tracker) }).first;
+					auto vrpn_tracker = std::make_shared<vrpn_Tracker_AsterTrack>(camera->id, path.c_str(), io.vrpn.server.get());
+					io_tracker = io.vrpn.trackers.insert({ camera->id, std::move(vrpn_tracker) }).first;
 				}
 				// Send static position of cameras as reference for clients
 				// TODO: Implement proper custom protocol for meta-information like cameras, single 3D markers, etc.
@@ -161,19 +161,19 @@ void IntegrationsUpdate(IntegrationsState &state, ServerState &server)
 		}
 
 		LOG(LIO, LTrace, "Updating individual trackers!\n");
-		for (auto &trackerIO : state.vrpn.trackers)
+		for (auto &trackerIO : io.vrpn.trackers)
 		{ // Handle mainloop (mostly ping-pong) here as well
 			trackerIO.second->mainloop();
 		}
 	}
 
-	if (state.vmc.enabled && vmc_try_open(state.vmc.output, state.vmc.host))
+	if (io.vmc.enabled && vmc_try_open(io.vmc.output, io.vmc.host))
 	{ // Try opening UDP port if not already done
 		if (updateCameras)
 		{ // Send cameras as trackers to give clients an opportunity to display them as references
 			std::vector<vmc_device> cameras;
-			cameras.reserve(server.pipeline.cameras.size());
-			for (const auto &camera : server.pipeline.cameras)
+			cameras.reserve(state.pipeline.cameras.size());
+			for (const auto &camera : state.pipeline.cameras)
 			{
 				cameras.emplace_back(VMCRole::Tracker,
 					asprintf_s("AsterTrack_Camera_%d", camera->index),
@@ -181,37 +181,37 @@ void IntegrationsUpdate(IntegrationsState &state, ServerState &server)
 					(float)getEffectiveFoVD(camera->calib, camera->mode)
 				);
 			}
-			vmc_send_device_packets(state.vmc.output, cameras, timestamp, dtS(server.lastStreamingStart, timestamp));
+			vmc_send_device_packets(io.vmc.output, cameras, timestamp, dtS(state.lastStreamingStart, timestamp));
 		}
 	}
 }
 
-void IntegrationsReceive(IntegrationsState &state, const ServerState &server)
+void IntegrationsReceive(IntegrationsState &io, const ServerState &state)
 {
-	auto io_lock = std::unique_lock(state.mutex);
+	auto io_lock = std::unique_lock(io.mutex);
 
-	if (state.vrpn.enabled)
+	if (io.vrpn.enabled)
 	{ // Fetch incoming packets
 		LOG(LIO, LTrace, "Updating VRPN server connection to fetch IMU packets!\n");
-		state.vrpn.server->mainloop();
-		if (!state.vrpn.server->doing_okay())
+		io.vrpn.server->mainloop();
+		if (!io.vrpn.server->doing_okay())
 			LOG(LIO, LWarn, "VRPN Connection Error!\n");
 		else
 			LOG(LIO, LTrace, "     Server connection is doing ok!\n");
 	}
 }
 
-void IntegrationsSendTracker(IntegrationsState &state, TrackerOutput &tracker, const TrackerOutputData &data, TimePoint_t time)
+void IntegrationsSendTracker(IntegrationsState &io, TrackerOutput &tracker, const TrackerOutputData &data, TimePoint_t time)
 {
-	if (!state.vrpn.enabled && !state.vmc.enabled) return;
+	if (!io.vrpn.enabled && !io.vmc.enabled) return;
 
-	auto io_lock = std::unique_lock(state.mutex);
+	auto io_lock = std::unique_lock(io.mutex);
 
-	if (tracker.vrpn && state.vrpn.lowLatencySend)
+	if (tracker.vrpn && io.vrpn.lowLatencySend)
 	{ // Fast-track output to VRPN
 		// TODO: Not thread-safe?
 		tracker.vrpn->updatePose(0, data.processedTime, data.processedPose);
-		state.vrpn.server->send_pending_reports();
+		io.vrpn.server->send_pending_reports();
 	}
 
 	// Pose may have been post-processed for realtime output, e.g. with extra filtering
@@ -226,11 +226,11 @@ void IntegrationsSendTracker(IntegrationsState &state, TrackerOutput &tracker, c
 	tracker.processed.push(data);
 }
 
-void IntegrationsSendFrame(IntegrationsState &state, ServerState &server, std::shared_ptr<FrameRecord> &frame)
+void IntegrationsSendFrame(IntegrationsState &io, ServerState &state, std::shared_ptr<FrameRecord> &frame)
 {
-	auto io_lock = std::unique_lock(state.mutex);
+	auto io_lock = std::unique_lock(io.mutex);
 
-	bool vmc_connected = state.vmc.enabled && vmc_is_opened(state.vmc.output);
+	bool vmc_connected = io.vmc.enabled && vmc_is_opened(io.vmc.output);
 	std::vector<vmc_device> vmc_output;
 	if (vmc_connected)
 		vmc_output.reserve(frame->trackers.size());
@@ -240,7 +240,7 @@ void IntegrationsSendFrame(IntegrationsState &state, ServerState &server, std::s
 		if (!trackRecord.result.isDetected() && !trackRecord.result.isTracked()) continue;
 		auto trackConfig = std::find_if(state.trackerConfigs.begin(), state.trackerConfigs.end(),
 			[&](auto &t){ return t.id == trackRecord.id; }); */
-	for (auto &outputIt : server.trackerOutput)
+	for (auto &outputIt : state.trackerOutput)
 	{
 		TrackerOutput &output = outputIt.second;
 		if (output.processed.empty()) continue;
@@ -251,10 +251,10 @@ void IntegrationsSendFrame(IntegrationsState &state, ServerState &server, std::s
 		auto &frameOutput = output.processed.front();
 
 		// Already sent via vrpn in IntegrationsSendTracker
-		if (state.vrpn.enabled && !state.vrpn.lowLatencySend)
+		if (io.vrpn.enabled && !io.vrpn.lowLatencySend)
 		{
-			auto io_tracker = state.vrpn.trackers.find(output.id);
-			if (io_tracker != state.vrpn.trackers.end())
+			auto io_tracker = io.vrpn.trackers.find(output.id);
+			if (io_tracker != io.vrpn.trackers.end())
 				io_tracker->second->updatePose(0, frameOutput.processedTime, frameOutput.processedPose);
 		}
 
@@ -270,17 +270,17 @@ void IntegrationsSendFrame(IntegrationsState &state, ServerState &server, std::s
 		output.processed.pop();
 	}
 
-	if (state.vrpn.enabled)
+	if (io.vrpn.enabled)
 	{
 		LOG(LIO, LTrace, "Updating VRPN server connection to push tracker packets!\n");
-		state.vrpn.server->send_pending_reports();
-		if (!state.vrpn.server->doing_okay())
+		io.vrpn.server->send_pending_reports();
+		if (!io.vrpn.server->doing_okay())
 			LOG(LIO, LDarn, "    A VRPN connection is facing issues!\n");
 	}
 
 	if (vmc_connected)
 	{
 		LOG(LIO, LTrace, "Pushing tracker packets to VMC output port!\n");
-		vmc_send_device_packets(state.vmc.output, vmc_output, frame->time, dtS(server.lastStreamingStart, frame->time));
+		vmc_send_device_packets(io.vmc.output, vmc_output, frame->time, dtS(state.lastStreamingStart, frame->time));
 	}
 }
